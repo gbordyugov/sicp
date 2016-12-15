bbc32f149424e30be3ac222b9c8b7a0dcc5f8cbc
Grisha Bordyugov <gregbg@gmail.com>
2016-12-15 23:26:30 +0100

    exercise 1.20

--- chapter1/chapter1.02.scm
+++ chapter1/chapter1.02.scm
@@ -311,7 +311,36 @@
 (fib 7)
 
 
+;;
+;; exercise 1.20
+;;
+
 (define (gcd a b)
   (if (= b 0)
     a
     (gcd b (remainder a b))))
+
+;;
+;; Applicative order:
+;;
+;; (gcd 206 40)
+;; (gcd 40 (remainder 206 40))
+;; (gcd 40 6)
+;; (gcd 6 (remainder 40 6))
+;; (gcd 6 4)
+;; (gcd 4 (remainder 6 4))
+;; (gcd 4 2)
+;; (gcd 2 (remainder 4 2))
+;; (gcd 2 0)
+;; 2
+;; four calls to remainder
+
+;;
+;; Normal (lazy) order
+;;
+;; (gcd 206 40)
+;; (gcd 40 (remainder 206 40))
+;; (if (= (remainder 206 40) 0)
+;;   40
+;;   (gcd (remainder 206 40) (remainder 40 (remainder 206 40))))
+;; one extra evaluation for (remainder 206 40) for the if condition
