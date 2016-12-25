a8077f708deee5fd061dabb1d908e8d721160b8f
Grigory Bordyugov <gregbg@gmail.com>
2016-12-25 21:39:48 +0100

    exercise 1.35

--- chapter1/chapter1.03.scm
+++ chapter1/chapter1.03.scm
@@ -202,4 +202,79 @@
 (f (lambda (z) (* z (+ z 1))))
 
 (f f) ;; cannot call 2 on 2
+
+
+
+
 ;;
+;; midpoint method
+;;
+
+(define (search f neg-point pos-point)
+  (define (close-enough? x y) (< (abs (- x y)) 0.001))
+  (define (average x1 x2) (/ (+ x1 x2) 2))
+  (let ((midpoint (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+      midpoint
+      (let ((test-value (f midpoint)))
+        (cond ((positive? test-value)
+               (search f neg-point midpoint))
+              ((negative? test-value)
+               (search f midpoint pos-point))
+              (else midpoint))))))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value) (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value) (positive? a-value))
+           (search f b a))
+          (else
+            (error "Values are not of opposite sign" a b)))))
+
+(half-interval-method sin 2.0 4.0)
+
+(half-interval-method (lambda (x) (- (* x x x) (* 2 x) 3))
+                      1.0
+                      2.0)
+
+;;
+;; fixed points of functions
+;;
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(fixed-point cos 1.0)
+
+(fixed-point (lambda (y) (+ (sin y) (cos y))) 1.0)
+
+(define (ff-sqrt x)
+  (fixed-point (lambda (y) (/ x y)) 1.0))
+
+(ff-sqrt 625.0) ;; this doesn't converge
+
+(define (average x1 x2) (/ (+ x1 x2) 2))
+
+(average 2.0 3.0)
+
+(define (ff-sqrt x)
+  (fixed-point (lambda (y) (average y (/ x y))) 1.0))
+
+(ff-sqrt 625.0) ;; now it works
+
+
+;;
+;; exercise 1.35
+;;
+
+(define golden-ratio (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
