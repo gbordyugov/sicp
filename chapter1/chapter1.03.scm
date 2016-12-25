8f069fcbd06078ddb0a53c91b59a7293647a2f3d
Grigory Bordyugov <gregbg@gmail.com>
2016-12-25 21:55:57 +0100

    exercise 1.36

--- chapter1/chapter1.03.scm
+++ chapter1/chapter1.03.scm
@@ -278,3 +278,32 @@
 ;;
 
 (define golden-ratio (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+
+
+;;
+;; exercise 1.36
+;;
+
+(define (fixed-point f first-guess)
+  (define tolerance 0.00001)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (newline)
+    (display "guess: ")
+    (display guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+        next
+        (try next))))
+  (try first-guess))
+
+(define solution1
+  (fixed-point (lambda (x)
+                 (/ (log 1000) (log x)))
+               3.0))
+
+(define solution2
+  (fixed-point (lambda (x)
+                 (average x (/ (log 1000) (log x))))
+               3.0))
