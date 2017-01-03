6816f7cd4f0c45953a0f82df9c9d1989c40e7caf
Grisha Bordyugov <gregbg@gmail.com>
2017-01-03 19:57:06 +0100

    exercise 2.3

--- chapter2/chapter2.01.scm
+++ chapter2/chapter2.01.scm
@@ -27,6 +27,7 @@
 
 (define (print-rat x)
   (newline)
+  (display "rat ")
   (display (numer x))
   (display "/")
   (display (denom x)))
@@ -56,3 +57,38 @@
 (print-rat (make-rat -1  2))
 
 (print-rat (make-rat -1 -2))
+
+
+;;
+;; exercise 2.2
+;;
+
+(define  make-segment cons)
+(define start-segment car)
+(define   end-segment cdr)
+
+(define make-point cons)
+(define x-point car)
+(define y-point cdr)
+
+(define (print-point p)
+  (newline)
+  (display "point (")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define (midpoint-segment s)
+  (define (avg a b) (/ (+ a b) 2))
+  (let* ((p1 (start-segment s))
+         (p2 ( end-segment s))
+         (x1 (x-point p1))
+         (y1 (y-point p1))
+         (x2 (x-point p2))
+         (y2 (y-point p2)))
+    (make-point (avg x1 x2) (avg y1 y2))))
+
+(define p1 (make-point 1.0 2.0))
+(define p2 (make-point 2.0 1.0))
+(print-point (midpoint-segment (make-segment p1 p2)))
