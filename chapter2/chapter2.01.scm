0fd5789de0e35d4b759248281ab4d7929b3d28d7
Grisha Bordyugov <gregbg@gmail.com>
2017-01-03 19:44:32 +0100

    exercise 2.1

--- chapter2/chapter2.01.scm
+++ chapter2/chapter2.01.scm
@@ -1,4 +1,58 @@
 (define (add-rat x y)
-  (make-rate (+ (* (number x) (denom y))
-                (* (number y) (denom x)))
-             (* (denom x) (denom y))))
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (make-rat n d) (cons n d))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+   (cons (/ n g) (/ d g))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+(print-rat one-half)
+
+(define one-third (make-rat 1 3))
+(print-rat (add-rat one-half one-third))
+(print-rat (mul-rat one-half one-third))
+(print-rat (add-rat one-third one-third))
+
+;;
+;; exercise 2.1
+;;
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+   (if (> (* n d) 0)
+     (cons (abs n) (abs d))
+     (cons (- (abs n)) (abs d)))))
+
+(print-rat (make-rat  1  2))
+
+(print-rat (make-rat  1 -2))
+
+(print-rat (make-rat -1  2))
+
+(print-rat (make-rat -1 -2))
