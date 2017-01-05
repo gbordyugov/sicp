7b22195a3fd3f5d1dd17af60557c1460c1124093
Grisha Bordyugov <gregbg@gmail.com>
2017-01-05 19:49:05 +0100

    nasty bug in exercise 2.7

--- chapter2/chapter2.01.scm
+++ chapter2/chapter2.01.scm
@@ -279,8 +279,8 @@
 ;;
 
 (define (make-interval a b) (cons a b))
-(define (upper-bound i) (car i))
-(define (lower-bound i) (cdr i))
+(define (upper-bound i) (cdr i))
+(define (lower-bound i) (car i))
 
 (define (sub-interval x y)
   (mul-interval x (make-interval (- (upper-bound y))
