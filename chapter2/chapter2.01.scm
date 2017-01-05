2032901d4b8262c09079574179a7cc994eb8b96e
Grisha Bordyugov <gregbg@gmail.com>
2017-01-05 19:51:58 +0100

    another typogit diff

--- chapter2/chapter2.01.scm
+++ chapter2/chapter2.01.scm
@@ -268,7 +268,7 @@
         (p2 (* (lower-bound x) (upper-bound y)))
         (p3 (* (upper-bound x) (lower-bound y)))
         (p4 (* (upper-bound x) (upper-bound y))))
-    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 pe))))
+    (make-interval (min p1 p2 p3 p4) (max p1 p2 p3 p4))))
 
 (define (div-interval x y)
   (mul-interval x (make-interval (/ 1.0 (upper-bound y))
