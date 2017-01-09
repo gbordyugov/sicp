c706040da1623cc46b07a2a7dca77231fc25e49d
Grigory Bordyugov <gregbg@gmail.com>
2017-01-09 16:52:19 +0100

    exercise 2.25 done

--- chapter2/chapter2.02.scm
+++ chapter2/chapter2.02.scm
@@ -177,4 +177,4 @@
 (car (cdr (car (cdr (cdr (list 1 3 (list 5 7) 9))))))
 (car (car '((7))))
 
-(cdr '(1 (2 (3 (4 (5 (6 7)))))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
