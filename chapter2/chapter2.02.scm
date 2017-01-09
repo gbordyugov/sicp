78ecaf70001b456fc390cea3e3cb1b21cbed4508
Grigory Bordyugov <gregbg@gmail.com>
2017-01-09 16:53:39 +0100

    exercise 2.26 done

--- chapter2/chapter2.02.scm
+++ chapter2/chapter2.02.scm
@@ -178,3 +178,14 @@
 (car (car '((7))))
 
 (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1 (2 (3 (4 (5 (6 7))))))))))))))))))
+
+;;
+;; exercise 2.26
+;;
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(append x y)
+(cons x y)
+(list x y)
