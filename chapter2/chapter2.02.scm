2989c55cbc521d3b62a2ef07c2cfbe8eb292f504
Grisha Bordyugov <gregbg@gmail.com>
2017-01-21 21:12:14 +0100

    exercise 2.36

--- chapter2/chapter2.02.scm
+++ chapter2/chapter2.02.scm
@@ -427,3 +427,16 @@
 (new-count-leaves '(1))
 
 (new-count-leaves '((1 2) 3 ((3 4 (5 6)) 4 5)))
+
+
+;;
+;; exercise 2.36
+;;
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+    '()
+    (cons (accumulate   op init (map car seqs))
+          (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
