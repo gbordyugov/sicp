8fcca465d3dcb9caee45a172042731e058e02c40
Grisha Bordyugov <gregbg@gmail.com>
2017-01-29 18:30:18 +0100

    exercise 2.40

--- chapter2/chapter2.02.scm
+++ chapter2/chapter2.02.scm
@@ -570,3 +570,16 @@
 ;;
 ;; exercise 2.40
 ;;
+
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+             (map (lambda (j) (list i j))
+             (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(unique-pairs 3)
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum (filter prime-sum? (unique-pairs n))))
+
+(prime-sum-pairs 13)
