e2e67fc4d3a16329b662786e097066dc700f6511
Grisha Bordyugov <gregbg@gmail.com>
2017-01-29 18:52:00 +0100

    typed in the queens func of exercise 2.42

--- chapter2/chapter2.02.scm
+++ chapter2/chapter2.02.scm
@@ -607,3 +607,25 @@
           (unique-triples n)))
 
 (sum-triples 5 6)
+
+
+;;
+;; exercise 2.42 - the dreaded queens puzzle!
+;;
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+      (list empty-board)
+      (filter
+        (lambda (positions) (safe? k positions))
+        (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position
+                     new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+          
+
