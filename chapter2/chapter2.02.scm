61eaedf020c429cdf77518d5ab29523e1132112b
Grigory Bordyugov <gregbg@gmail.com>
2017-02-12 22:42:48 +0100

    working on exercise 2.43

--- chapter2/chapter2.02.scm
+++ chapter2/chapter2.02.scm
@@ -688,3 +688,29 @@
 (safe? 8 '(1 1))
 
 (queens 8)
+
+
+;;
+;; execise 2.43
+;;
+
+(define (queens-slow board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+      (list empty-board)
+      (filter
+        (lambda (positions) (safe? k positions))
+        (flatmap
+          (lambda (new-row)
+            (map (lambda (rest-of-queens)
+                   (adjoin-position new-row k rest-of-queens))
+                 (queen-cols (- k 1))))
+        (enumerate-interval 1 board-size)))))
+  (queen-cols board-size))
+
+(queens-slow 6)
+
+;;
+;; todo: explanation why the code is slow
+;;       time estimation
+;;
