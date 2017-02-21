df10da95747cc2b91be019ac0c576704775d40b1
Grigory Bordyugov <gregbg@gmail.com>
2017-02-21 09:41:01 +0100

    exercise 2.56

--- chapter2/chapter2.03.scm
+++ chapter2/chapter2.03.scm
@@ -47,3 +47,88 @@
 (car ''abracadabra)
 ;; = (car '(quote abracadabra)))
 ;; = quote
+
+
+;;
+;; Symbolic differentiation
+;;
+
+;;
+;; considered as provided
+;;
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+           (make-product (multiplier exp)
+                         (deriv (multiplicand exp) var))
+           (make-product (deriv (multiplier exp) var)
+                         (multiplicand exp))))
+        (else
+          (error "unknown expression type: DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+(define (make-sum     a1 a2) (list '+ a1 a2))
+
+(define (make-product m1 m2) (list '* m1 m2))
+
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+
+(define (addend s) ( cadr s))
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+
+(define (multiplier   s) ( cadr s))
+(define (multiplicand s) (caddr s))
+
+(deriv '(+ x 3) 'x)
+
+(deriv '(* x y) 'x)
+
+(deriv '(* (* x y) (+ x 3)) 'x)
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (=number? exp num) (and (number? exp) (= exp num)))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+;;
+;; exercise 2.56
+;;
+
+(define (base     exp) ( cadr exp))
+(define (exponent exp) (caddr exp))
+
+;; (define (make-exponentiation base exp) (list '** base exp))
+
+(define (exponentiation? e)
+  (eq? (car e) '**))
+
+(define (make-exponentiation base exp)
+  (cond ((=number? exp 0) 1)
+        ((=number? exp 1) base)
+        ((and (number? base) (number? exp))
+         (expt base exp))
+        (else (list '** base exp))))
