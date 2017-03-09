d4ae9a0f0ab4f83b395ac51948b91f70772f4069
Grigory Bordyugov <gregbg@gmail.com>
2017-03-09 15:44:37 +0100

    exercise 2.63

--- chapter2/chapter2.03.scm
+++ chapter2/chapter2.03.scm
@@ -469,3 +469,118 @@
                        (cons y (union-set set1 ys))))))))
 
 (union-set '(1 2 3) '(3 4 5))
+
+;;
+;; Set as binary trees
+;;
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
+
+(adjoin-set 3 (make-tree 5 '() '()))
+
+
+;;
+;; exercise 2.63
+;;
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+    '()
+    (append (tree->list-1 (left-branch tree))
+            (cons (entry tree)
+                  (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+      result-list
+      (copy-to-list (left-branch tree)
+                    (cons (entry tree)
+                          (copy-to-list (right-branch tree) result-list)))))
+  (copy-to-list tree '()))
+
+(define tree-a (make-tree 7
+                           (make-tree 3
+                                      (make-tree 1 
+                                                 () 
+                                                 ())
+                                      (make-tree 5
+                                                 '()
+                                                 '()))
+                           (make-tree 9
+                                      '()
+                                      (make-tree 11
+                                                 '()
+                                                 '()))))
+
+(define tree-b (make-tree 3
+                          (make-tree 1
+                                     '()
+                                     '())
+                          (make-tree 7
+                                     (make-tree 5
+                                                '()
+                                                '())
+                                     (make-tree 9
+                                                '()
+                                                (make-tree 11
+                                                           '()
+                                                           '())))))
+
+(define tree-c (make-tree 5
+                          (make-tree 3
+                                     (make-tree 1
+                                                '()
+                                                '())
+                                     '())
+                          (make-tree 9
+                                     (make-tree 7
+                                                '()
+                                                '())
+                                     (make-tree 11
+                                                '()
+                                                '()))))
+
+(tree->list-1 tree-a)
+
+(tree->list-2 tree-a)
+
+(tree->list-1 tree-b)
+
+(tree->list-2 tree-b)
+
+(tree->list-1 tree-c)
+
+(tree->list-2 tree-c)
+
+;;
+;; a) yes, they produce the same results
+;;
+;; b) no, one is tail recursive, and the other is just recursive
