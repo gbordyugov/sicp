dde8552a95175af4e243c697c55958e0d0df60cc
Grigory Bordyugov <gregbg@gmail.com>
2017-03-10 10:21:28 +0100

    started Huffman encoding example

--- chapter2/chapter2.03.scm
+++ chapter2/chapter2.03.scm
@@ -655,3 +655,37 @@
         ((= key- (key (entry tree))) (entry tree))
         ((< key- (key (entry tree))) (lookup ( left-branch tree) key))
         ((> key- (key (entry tree))) (lookup (right-branch tree) key))))
+
+
+;;
+;; Example: Huffman Enconding Trees
+;;
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x)
+  (cadr x))
+
+(define (weight-leaf x)
+  (caddr x))
+
+(define (make-code-tree left right)
+  (list left right (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define ( left-branch tree) (car  tree))
+(define (right-branch tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+    (list (symbol-leaf tree))
+    (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+    (weight-leaf tree)
+    (cadddr tree)))
