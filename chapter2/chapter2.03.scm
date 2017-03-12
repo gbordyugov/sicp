c5623b6677faca8d59bc7399a8b72b9f2ee34794
Grisha Bordyugov <gregbg@gmail.com>
2017-03-12 20:24:40 +0100

    exercise 2.67

--- chapter2/chapter2.03.scm
+++ chapter2/chapter2.03.scm
@@ -719,3 +719,19 @@
      (adjoin-set (make-leaf (car  pair)    ; symbol
                             (cadr pair))   ; frequency
                  (make-leaf-set (cdr pairs))))))
+
+;;
+;; exericse 2.67
+;;
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                    (make-leaf 'B 2)
+                    (make-code-tree
+                      (make-leaf 'D 1)
+                      (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(decode sample-message sample-tree)
