c968e23e70f8a17aa9767f98c60a1ebecfa27484
Grisha Bordyugov <gregbg@gmail.com>
2017-03-12 20:21:49 +0100

    Huffman trees up to exercise 2.67

--- chapter2/chapter2.03.scm
+++ chapter2/chapter2.03.scm
@@ -689,3 +689,33 @@
   (if (leaf? tree)
     (weight-leaf tree)
     (cadddr tree)))
+
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+      '()
+      (let ((next-branch (choose-branch (car bits) current-branch)))
+       (if (leaf? next-branch)
+         (cons (symbol-leaf next-branch)
+               (decode-1 (cdr bits) tree))
+         (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch  branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit: CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+    '()
+    (let ((pair (car pairs)))
+     (adjoin-set (make-leaf (car  pair)    ; symbol
+                            (cadr pair))   ; frequency
+                 (make-leaf-set (cdr pairs))))))
