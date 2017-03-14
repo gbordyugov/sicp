2768bb26e365d6a94f38e85d31102c106b7ba4e9
Grigory Bordyugov <gregbg@gmail.com>
2017-03-14 09:32:11 +0100

    generic apply

--- chapter2/chapter2.04.scm
+++ chapter2/chapter2.04.scm
@@ -207,3 +207,22 @@
   (put 'make-from-mag-ang 'polar
        (lambda (r a) (tag (make-from-mag-ang   r a))))
   'done)
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contents args))
+        (error "No method for these types: APPLY-GENERIC"
+               (list op type-tags))))))
+
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle     z) (apply-generic 'angle     z))
+
+(define (make-from-real-imag x y)
+  ((get 'make-from-real-imag 'rectangular) x y))
+
+(define (make-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'polar) r a))
