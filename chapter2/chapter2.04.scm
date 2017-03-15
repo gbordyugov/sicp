44578c18cf7bde0dc8bbd389ea58a67f779f355d
Grigory Bordyugov <gregbg@gmail.com>
2017-03-15 14:49:27 +0100

    spaces in 2.73 (b) and (c)

--- chapter2/chapter2.04.scm
+++ chapter2/chapter2.04.scm
@@ -276,31 +276,25 @@
 ;;
 
 (define (install-deriv-package)
-
   ;; sum
   (define (make-sum x y) (list '+ x y))
   (define (addend s) (car  s)) ;; the op code is stripped by operands()
   (define (augend s) (cadr s))
-
   ;; product
   (define (make-product x y) (list '* x y))
   (define (multiplier   s) (car  s)) ;; the op code is stripped by operands()
   (define (multiplicand s) (cadr s))
-
   ;; derivative calculation
   (define (deriv-sum exp var)
     (make-sum (deriv (addend exp) var)
               (deriv (augned exp) var)))
-
   (define (deriv-product exp var)
     (make-sum (make-product (multiplier exp)
                             (deriv (multiplicand exp) var))
               (make-product (deriv (multiplier   exp) var)
                             (multiplicand exp))))
-
   (put 'deriv '+ deriv-sum)
   (put 'deriv '* deriv-product)
-
   'done)
 
 
@@ -309,33 +303,27 @@
 ;;
 
 (define (install-deriv-package)
-
   ;; sum
   (define (make-sum x y) (list '+ x y))
   (define (addend s) (car  s)) ;; the op code is stripped by operands()
   (define (augend s) (cadr s))
-
   ;; product
   (define (make-product x y) (list '* x y))
   (define (multiplier   s) (car  s)) ;; the op code is stripped by operands()
   (define (multiplicand s) (cadr s))
-
   ;; exponentiation
   (define (make-exponentiation x y) (list '** x y))
   (define (base exp)  ( car exp)) ;; the op code is stripped by operands()
   (define (poser exp) (cadr exp)) ;; the op code is stripped by operands()
-
   ;; derivative calculation
   (define (deriv-sum exp var)
     (make-sum (deriv (addend exp) var)
               (deriv (augned exp) var)))
-
   (define (deriv-product exp var)
     (make-sum (make-product (multiplier exp)
                             (deriv (multiplicand exp) var))
               (make-product (deriv (multiplier   exp) var)
                             (multiplicand exp))))
-
   (define (deriv-exponentiation exp var)
     (make-product (make-product (power exp)
                                 (make-exponentiation (base exp)
@@ -344,5 +332,4 @@
   (put 'deriv '+  deriv-sum)
   (put 'deriv '*  deriv-product)
   (put 'deriv '** deriv-exponentiation)
-
   'done)
