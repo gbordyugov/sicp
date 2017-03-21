a3141aee87e33e1e1714df6741f74e97ee384f38
Grigory Bordyugov <gregbg@gmail.com>
2017-03-21 17:51:47 +0100

    Hierarchies of types

--- chapter2/chapter2.05.scm
+++ chapter2/chapter2.05.scm
@@ -184,3 +184,36 @@
   'done)
 
 (define (=zero? x) (apply-generic '=zero? x y))
+
+;;
+;; Coercion
+;;
+
+(define (scheme-number->complex n)
+  (make-complex-from-real-imag (contents n) 0))
+
+(put-coercion 'scheme-number 'complex scheme-number->complex)
+
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contentes args))
+        (if (= (length args) 2)
+          (let ((type1 ( car type-tags))
+                (type2 (cadr type-tags))
+                (a1 ( car args))
+                (a2 (cadr args)))
+            (let ((t1->t2 (get-coercion type1 type2))
+                  (t2->t1 (get-coercion type2 type1)))
+              (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                    (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                    (else (error "No method for these types"
+                                 (list op type-tags))))))
+          (error "No method for these types"
+                 (list op type-tags)))))))
+
+;;
+;; Hierarchies of types
+;;
