00807cf92baadc5ddd3516211f799db5a7c5459e
Grigory Bordyugov <gregbg@gmail.com>
2017-03-21 09:25:26 +0100

    Section 2.5: Scheme number package

---
+++ chapter2/chapter2.05.scm
@@ -0,0 +1,27 @@
+;;
+;; Systems with Generic Operations
+;;
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number (lambda (x)
+                              (tag x)))
+  'done)
+
+;; note that this one is also polymorphic with dispatch on type
+(define (make-scheme-number n)
+  ((get 'make 'scheme-number) n))
