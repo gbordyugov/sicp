8f177a7f9e330868df747bbf793c8a5dab7a4717
Grigory Bordyugov <gregbg@gmail.com>
2017-03-22 09:49:50 +0100

    exercise 2.81 (c) done

--- chapter2/chapter2.05.scm
+++ chapter2/chapter2.05.scm
@@ -243,3 +243,23 @@
 ;; exercise 2.81 (c)
 ;;
 
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+        (apply proc (map contentes args))
+        (if (= (length args) 2)
+          (let ((type1 ( car type-tags))
+                (type2 (cadr type-tags))
+                (a1 ( car args))
+                (a2 (cadr args)))
+            (if (equal? type1 type2)
+              (error "Trying to coerce two equal types" type1 type2)
+              (let ((t1->t2 (get-coercion type1 type2))
+                    (t2->t1 (get-coercion type2 type1)))
+                (cond (t1->t2 (apply-generic op (t1->t2 a1) a2))
+                      (t2->t1 (apply-generic op a1 (t2->t1 a2)))
+                      (else (error "No method for these types"
+                                   (list op type-tags)))))))
+          (error "No method for these types"
+                 (list op type-tags)))))))
