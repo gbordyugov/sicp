ea5e67743b2a06d6c212d6395b5415bdb2c2fb1c
Grigory Bordyugov <gregbg@gmail.com>
2017-04-24 09:24:32 +0200

    started 2.5.3 Example: Symbolic Algebra

--- chapter2/chapter2.05.scm
+++ chapter2/chapter2.05.scm
@@ -483,3 +483,48 @@
 (define (sine   x) (apply-generic 'sine   x))
 (define (cosine x) (apply-generic 'cosine x))
 ;; define more functions here: tan, atan, hypot, etc
+
+
+;;
+;; 2.5.3 Example: Symbolic Algebra
+;;
+
+(define (add-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+                      (make-poly (variable p1)
+                                 (add-terms (term-list p1)
+                                            (term-list p2)))
+                      (error "Polys not in same var: ADD-POLY"
+                             (list p1 p2))))
+(define (mul-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+                      (make-poly (variable p1)
+                                 (mul-terms (term-list p1)
+                                            (term-list p2)))
+                      (error "Polys not in same var: ADD-POLY"
+                             (list p1 p2))))
+
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p)
+    (car p))
+  (define (term-list p)
+    (cdr p))
+  ;; procedures same-variable? and variable from section 2.3.2
+  ;; representation of terms and term list
+  ;; procedures adjoin-term ... coeff from text below
+  (define (add-poly p1 p2) (...))
+  (define (mul-poly p1 p2) (...))
+  ;; interface to rest of the system
+  (define (tag p)
+    (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (lambda (var terms) (tag (make-poly var terms))))
+  'done)
