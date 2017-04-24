395096e846ea3f9af3a1a4f91e5b526c1703d557
Grigory Bordyugov <gregbg@gmail.com>
2017-04-24 09:39:48 +0200

    mul-terms of 2.5.3

--- chapter2/chapter2.05.scm
+++ chapter2/chapter2.05.scm
@@ -562,3 +562,17 @@
                       (make-term (order t1)
                                  (add (coeff t1) (coeff t2)))
                       (add-terms (rest-terms L1) (rest-terms L2)))))))))
+
+(define (mul-terms L1 L2)
+  (if (empty-termlist? L1)
+    (the-empty-termlist)
+    (add-terms (mul-term-by-all-terms (first-term L1) L2)
+               (mul-terms (rest-terms L1) L2))))
+
+(define (mul-term-by-all-terms t1 L)
+  (if (empty-termlist? L)
+    (the-empty-termlist)
+    (let ((t2 (first-term L)))
+      (adjoin-term (make-term (+   (order t1) (order t2))
+                              (mul (coeff t1) (coeff t2)))
+                   (mul-term-by-all-terms t1 (rest-terms L))))))
