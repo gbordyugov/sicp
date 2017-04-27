5293e54282c4fd47ae5223524955d65bb5a3ce60
Grigory Bordyugov <gregbg@gmail.com>
2017-04-27 08:55:20 +0200

    exercise 2.93 done

--- chapter2/chapter2.05.scm
+++ chapter2/chapter2.05.scm
@@ -893,5 +893,54 @@
 ;;
 
 
+;;
+;; Extented exercise: Rational functions
+;;
+
+;;
+;; exercise 2.93
+;;
+
+(define (install-rational-package)
+  ;; internal procedures
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+  (define (make-rat n d)
+      (cons n g))
+  (define (equ-rat? x y)
+    (and (equ? (numer x) (numer y))
+         (equ? (denom x) (denom y))))
+  (define (=zero-rat? x)
+    (=zero? (numer x)))
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+  ;; interface to rest of the system
+  (define (tag x) (attach-tag 'rational x))
+  (put 'add '(rational rational)
+       (lambda (x y) (tag (add-rat  x y))))
+  (put 'sub '(rational rational)
+       (lambda (x y) (tag (sub-rat  x y))))
+  (put 'mul '(rational rational)
+       (lambda (x y) (tag (mul-rat  x y))))
+  (put 'div '(rational rational)
+       (lambda (x y) (tag (div-rat  x y))))
+  (put 'equ?   'rational equ-rat?)
+  (put '=zero? 'rational =zero-rat?)
+  (put 'make 'rational
+       (lambda (n d) (tag (make-rat n d))))
+  'done)
+
 
 
