ee964d271910e721131129539d888eed2dda2a06
Grigory Bordyugov <gregbg@gmail.com>
2017-04-28 09:14:37 +0200

    exercise 3.2 done

--- chapter3/chapter3.01.scm
+++ chapter3/chapter3.01.scm
@@ -16,3 +16,28 @@
 (acc1 3)
 
 (acc1 5)
+
+;;
+;; exercise 3.2
+;;
+
+(define (make-monitored f)
+  (let ((no-of-calls 0))
+    (begin
+      (define (monitored x)
+        (if (eq? x 'how-many-calls?)
+          no-of-calls
+          (begin
+            (set! no-of-calls (+ no-of-calls 1))
+            (f x))))
+      monitored)))
+
+(define s (make-monitored sqrt))
+
+(s 'how-many-calls?)
+(s 9)
+(s 9)
+(s 'how-many-calls?)
+(s 9)
+(s 16)
+(s 'how-many-calls?)
