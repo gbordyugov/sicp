7b2d616800600ebdd2efdbb6de09dc84268aa98e
Grigory Bordyugov <gregbg@gmail.com>
2017-04-28 09:02:44 +0200

    exercise 3.1 done

--- chapter3/chapter3.01.scm
+++ chapter3/chapter3.01.scm
@@ -1,3 +1,18 @@
 ;;
 ;; starting Chapter 3, hurrah!
 ;;
+
+;;
+;; exercise 3.1
+;;
+
+(define (make-accumulator value)
+  (lambda (to-add)
+    (set! value (+ value to-add))
+    value))
+
+(define acc1 (make-accumulator 10))
+
+(acc1 3)
+
+(acc1 5)
