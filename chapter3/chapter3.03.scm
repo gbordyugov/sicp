ee1723cdd9d9e900149805bdb3f9b4fdb206a28f
Grigory Bordyugov <gregbg@gmail.com>
2017-05-04 10:28:41 +0200

    exercise 3.16 done (a nice one)

--- chapter3/chapter3.03.scm
+++ chapter3/chapter3.03.scm
@@ -9,6 +9,8 @@
 ;; (cdr x) => (b)
 ;; (cdr x) => (b c d)
 
+
+
 ;;
 ;; exercise 3.13
 ;;
@@ -16,6 +18,8 @@
 ;;
 ;; a circular list list, (last-pair z) would produce an infinite loop
 ;;
+
+
 
 ;;
 ;; exercise 3.14
@@ -25,10 +29,39 @@
 ;; reverses list in-place
 ;;
 
+
+
 ;;
 ;; exercise 3.15
 ;;
 
 ;;
 ;; drawing in my notebook
+;;
+
+
+
+;;
+;; exercise 3.16
+;;
+
+(define (count-pairs x)
+  (if (not (pair? x))
+    0
+    (+ (count-pairs (car x))
+       (count-pairs (cdr x))
+       1)))
+
+;;
+;; 3 pairs is easy : just '(a b c)
+;;
+;; 4 pairs         : list of three with car pointing to the last pair,
+;;                   i.e. (define x '(a b c))
+;;                        (set-car! x (cddr x))
+;; 7 pairs         :  (define x '(a b c))
+;;                    (set-car! x       (cdr x))
+;;                    (set-car! (cdr x) (cddr x))
+
+;;
+;; never return at all: circular list
 ;;
