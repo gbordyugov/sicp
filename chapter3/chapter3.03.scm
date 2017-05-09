c5ca31996dd47dcef78596e03087dfbc7e9288ac
Grigory Bordyugov <gregbg@gmail.com>
2017-05-09 09:25:07 +0200

    two-dimensional tables

--- chapter3/chapter3.03.scm
+++ chapter3/chapter3.03.scm
@@ -435,3 +435,29 @@
 
 (define (make-table)
   (list '*table))
+
+;;
+;; Two-dimensional tables
+;;
+
+(define (lookup key-1 key-2 table)
+  (let ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+      (let ((record (assoc key-2 (cdr subtable))))
+        (if record
+          (cdr record)
+          #f))
+      #f)))
+
+(define (insert! key-1 key-2 value table)
+  (let ((subtable (asso key-1 (cdr table))))
+    (if subtable
+      (let ((record (assoc key-2 (cdr subtable))))
+        (if record
+          (set-cdr! record value)
+          (set-cdr! subtable (cons (cons key-2 value)
+                                   (cdr subtable)))))
+      (set-cdr! table (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr table)))))
+  'ok)
