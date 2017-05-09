8c311f2c454386f10922d940e4fff21f3dfb4000
Grigory Bordyugov <gregbg@gmail.com>
2017-05-09 09:44:48 +0200

    exercise 3.24

--- chapter3/chapter3.03.scm
+++ chapter3/chapter3.03.scm
@@ -424,7 +424,6 @@
   (cond ((null? records) false)
         ((equal? key (caar records)) (car records))
         (else (assoc key (cdr records)))))
-
 (define (insert! key value table)
   (let ((record (assoc key (cdr table))))
     (if record
@@ -432,9 +431,14 @@
       (set-cdr! table (cons (cons key value)
                             (cdr table)))))
   'ok)
-
 (define (make-table)
   (list '*table*))
+
+(define table (make-table))
+
+(insert! 'a 1 table)
+
+(lookup 'a  table)
 
 ;;
 ;; Two-dimensional tables
@@ -493,3 +497,54 @@
             ((eq? m 'insert-proc) insert!)
             (else (error "Unknown operation: TABLE" m))))
     dispatch))
+
+(define t (make-table))
+
+((t 'insert-proc) 'a 'b 3)
+
+((t 'insert-proc) 'a 'c 4)
+
+((t 'lookup-proc) 'a 'b)
+
+((t 'lookup-proc) 'a 'c)
+
+((t 'lookup-proc) 'd 'c)
+
+
+;;
+;; exercise 3.24
+;;
+
+(define (make-table same-key?)
+  (define (assoc key records)
+    (cond ((null? records) false)
+          ((same-key? key (caar records)) (car records))
+          (else (assoc key (cdr records)))))
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+          (let ((record (assoc key-2 (cdr subtable))))
+            (if record
+              (cdr record)
+              #f))
+          #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+          (let ((record (assoc key-2 (cdr subtable))))
+            (if record
+              (set-cdr! record value)
+              (set-cdr! subtable (cons (cons key-2 value)
+                                       (cdr subtable)))))
+          (set-cdr! local-table
+                    (cons (list key-1 (cons key-2 value))
+                          (cdr local-table)))))
+          'ok)
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc) insert!)
+            (else (error "Unknown operation: TABLE" m))))
+    dispatch))
+
+(define t (make-table))
