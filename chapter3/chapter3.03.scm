52ccce770de2db8d35c41482a1238c256f08bc13
Grigory Bordyugov <gregbg@gmail.com>
2017-05-12 09:29:01 +0200

    starting the circuit simulator chapter

--- chapter3/chapter3.03.scm
+++ chapter3/chapter3.03.scm
@@ -712,3 +712,60 @@
 ;;
 ;; 3.3.4 A Simulator for Digital Circuits
 ;;
+
+(define (half-adder a b s c)
+  (let ((d (make-wire))
+        (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s  (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum  c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+;;
+;; (get-signal <wire>)
+;; returns the current value of the signal on the wire
+;;
+;; (set-signal! <wire> <new value>)
+;; changes the value of the signal on the wire to the new value
+;;
+;; (add-action! <wire> <procedure of no arguments>)
+;; asserts that the designated procedure should be run whenever the
+;; signal on the wire changes value
+;;
+;; (after-delay <tau> <procedure of no arguments>)
+;; executes the procedure after delay tau
+
+(let (inverter input output)
+  (define (invert-output)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "invalid signal in logical-not" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value (logical-and (get-signal a1)
+                                  (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda () (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure) ;; whichever of both changes,
+  (add-action! a2 and-action-procedure) ;; we have to run the proc
+  'ok)
+
+
