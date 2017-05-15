4f2039dc64b6c098e9eb262616315e2615da4d9e
Grigory Bordyugov <gregbg@gmail.com>
2017-05-15 09:56:34 +0200

    A sample simulation

--- chapter3/chapter3.03.scm
+++ chapter3/chapter3.03.scm
@@ -911,3 +911,30 @@
       (remove-first-agenda-item! the-agenda)
       (propagage))))
 
+;;
+;; A sample simulation
+;;
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name) (display " ")
+                 (display (current-time the-agenda))
+                 (display "  New-value = ")
+                 (display (get-signal wire)))))
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define  or-gate-delay 5)
+
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define sum     (make-wire))
+(define carry   (make-wire))
+
+;; (probe 'sum   sum)
+;; (probe 'carry carry)
+
+
