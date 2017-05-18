387b66f39ac37cbdee8bbb7a2b1997ed23cf477f
Grigory Bordyugov <gregbg@gmail.com>
2017-05-18 11:43:36 +0200

    started exercise 3.35

--- chapter3/chapter3.03.scm
+++ chapter3/chapter3.03.scm
@@ -1193,3 +1193,18 @@
 ;; computing a
 ;;
 
+;;
+;; exercise 3.35
+;;
+
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+      (if (< (get-value b) 0)
+        (error "square less than 0: SQUARER" (get-value b))
+        (set-value! a (sqrt b) me)))
+    (if (has-value? a)
+      (set-value! b (* a a) me)))
+  (define (process-forget-value)
+    (forget-value! a me)
+
