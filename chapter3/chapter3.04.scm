d725d9a7984a568b58287543e3b98e726ab6e593
Grigory Bordyugov <gregbg@gmail.com>
2017-05-19 10:10:34 +0200

    exercise 3.39 done

--- chapter3/chapter3.04.scm
+++ chapter3/chapter3.04.scm
@@ -5,3 +5,11 @@
 ;; basically, there are 6 permutations of 3 elemenents, I trust I can
 ;; do the arythmetics
 ;;
+
+;;
+;; exercise 3.39
+;;
+;; it's three possibilities, depending whether the second procedure
+;; runs a) before the first serialized lambda b) between it and
+;; assignment to x in the first procedure and c) after both
+;;
