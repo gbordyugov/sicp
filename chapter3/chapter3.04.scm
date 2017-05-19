cfd91e48cbec303cb7e0030f503bd8258c87ea2f
Grigory Bordyugov <gregbg@gmail.com>
2017-05-19 10:13:20 +0200

    exercise 3.40

--- chapter3/chapter3.04.scm
+++ chapter3/chapter3.04.scm
@@ -13,3 +13,12 @@
 ;; runs a) before the first serialized lambda b) between it and
 ;; assignment to x in the first procedure and c) after both
 ;;
+
+
+;;
+;; exercise 3.40
+;;
+;; both serialized funcs commute and the computation would produce the
+;; same result independently of the order the serialized funcs are
+;; called in.
+;;
