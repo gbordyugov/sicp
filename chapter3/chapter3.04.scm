4f6399a719da9c14b092ea829636e6b18ac1e8a0
Grigory Bordyugov <gregbg@gmail.com>
2017-05-22 09:21:06 +0200

    exercise 3.43 done

--- chapter3/chapter3.04.scm
+++ chapter3/chapter3.04.scm
@@ -38,3 +38,23 @@
 ;; fly in each call of dispatch. Here, we pre-cache them. Seems OK to
 ;; me
 ;;
+
+
+;;
+;; exercise 3.43
+;;
+;;
+;; in the sequential version, all possible single exchanges preserve
+;; the property of having a combination of $10, $20, and $30 as
+;; accounts' balances
+;;
+;; a = 10, b = 20, c = 30
+;; run exchange(a, b) and exchange(b, c) in parallel
+;; calculate delta(a, b) = 10
+;; complete exchange(b, c) such that b = 30 and c = 20
+;; add 10 to a, producing 20, subtract 10 from b, producing 20
+;; end up with a = 20, b = 20, c = 30, see that the sum remained
+;; preserved
+;;
+;; the sum remains preserved since both deposit and withdraw
+;; operations are atomic (serialized)
