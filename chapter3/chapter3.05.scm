a3a433a648e6e79805b5722dcb14632a81edc6a0
Grigory Bordyugov <gregbg@gmail.com>
2017-05-24 09:53:48 +0200

    started reading the streams chapter

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -3,3 +3,32 @@
 ;; construction time. With streams, the `cdr` is evaluated at
 ;; selection time.
 ;;
+
+
+;;
+;; the two stream axioms:
+;;
+;; (stream-car (cons-stream x y)) = x
+;; (stream-cdr (cons-stream x y)) = y
+;;
+;;
+;; (cons-stream <a> <b>)
+;; is equivalent to
+;; (cons <a> (delay <b>))
+;;
+;; (define (stream-car s) (car s))
+;; (define (stream-cdr s) (force (cdr stream)))
+
+(define (memo-proc proc)
+  (let ((already-run? false)
+        (result       false))
+    (lambda ()
+      (if (not already-run?)
+        (begin
+          (set! result (proc))
+          (set! already-run true)
+          result)
+        result))))
+
+;; so (delay <exp>) is equiavalent to
+;; (memo-proc (labda () <exp>))
