0f44b68484c4be2047b3e14684d1958fd8cd2af6
Grigory Bordyugov <gregbg@gmail.com>
2017-05-24 10:18:29 +0200

    some addition to stream functions

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -19,6 +19,31 @@
 ;; (define (stream-car s) (car s))
 ;; (define (stream-cdr s) (force (cdr stream)))
 
+(define (stream-ref s n)
+  (if (= n 0)
+    (stream-car s)
+    (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-map proc s)
+  (if (stream-null? s)
+    the-empty-stream
+    (cons-stream (proc (stream-car s))
+                 (stream-map proc (stream-cdr s)))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+    'done
+    (begin
+      (proc (stream-car s))
+      (stream-for-each proc (stream-cdr s)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
 (define (memo-proc proc)
   (let ((already-run? false)
         (result       false))
@@ -40,7 +65,7 @@
 (define (stream-map proc . argstreams)
   (if (null? (car argstreams))
     the-empty-stream
-    (stream-cons
+    (cons-stream
       (apply proc       (map stream-car argstreams))
       (apply stream-map (cons proc (map (stream-cdr) argstreams))))))
 
