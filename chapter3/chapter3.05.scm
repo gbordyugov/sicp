38284f730a774d7c60b5d9b8fbcfa56f297b7275
Grigory Bordyugov <gregbg@gmail.com>
2017-05-26 09:38:33 +0200

    Defining streams implicitly

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -117,3 +117,34 @@
                  (not (divisible? x n)))
                (stream-cdr stream))))))
 (define primes (sieve (integers-starting-from 2)))
+
+;;
+;; Definining streams implicitly
+;;
+
+(define ones (cons-stream 1 ones))
+
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define integers
+  (cons-stream 1 (add-streams one integers)))
+
+(define fibs
+    (cons-stream 0 (cons-stream 1 (add-streams (stream-cdr fibs) fibs))))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define double (cons-stream (scale-stream double 2)))
+
+(define primes
+  (cons-stream
+    2
+    (stream-filter prime? (integers-starting-from 3))))
+
+(define (prime? n)
+  (define (iter ps)
+    (cond ((> (square (stream-car ps)) n) true)
+          ((divisible? n (stream-car ps)) false)
+          (else (iter (stream-cdr ps)))))
+  (iter primes))
