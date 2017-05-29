afbb741f849afc6006151ae6c3ec6442a7c0d74b
Grigory Bordyugov <gregbg@gmail.com>
2017-05-29 08:45:17 +0200

    exercise 3.54 done

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -167,5 +167,5 @@
 
 (define (mul-streams s1 s2) (stream-map * s1 s2))
 
-(define factorials ;; not yet workin ... ;-(
-  (cons-stream 1 (mul-streams integers (stream-cdr factorials))))
+(define factorials
+  (cons-stream 1 (mul-streams factorials integers)))
