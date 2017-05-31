177a73b9cb37af1cd5f4fcfd320827d3d806cadf
Grigory Bordyugov <gregbg@gmail.com>
2017-05-31 10:31:12 +0200

    started 3.5.3, Euler transform

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -327,3 +327,36 @@
 (define tan-series (div-series sin-series cos-series))
 
 (stream-ref tan-series 0)
+
+
+
+;;
+;; 3.5.3 Exploiting the Stream Paradigm
+;;
+
+(define (sqrt-improve guess x)
+  (/ (+ guess (/ x guess)) 2))
+
+(define (sqrt-stream x)
+  (define guesses
+    (cons-stream
+      1.0
+      (stream-map (lambda (guess) (sqrt-improve guess x)) guesses)))
+  guesses)
+
+;; (display-stream (sqrt-stream 2))
+
+(define (pi-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (pi-summands (+ n 2)))))
+
+(define pi-stream
+  (scale-stream (partial-sums (pi-summands 1)) 4))
+
+(define (euler-transform s)
+  (let ((s0 (stream-ref s 0))
+        (s1 (stream-ref s 1))
+        (s2 (stream-ref s 2)))
+    (cons-stream (- s2 (/ (square (- s2 s1))
+                          (+ s0 (* -2 s1) s2)))
+                 (euler-transform (stream-cdr s)))))
