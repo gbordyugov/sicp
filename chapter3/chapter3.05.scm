58e32b4768ccb4e447bc37e662e25f7fc3747084
Grigory Bordyugov <gregbg@gmail.com>
2017-06-01 09:34:37 +0200

    accelerated streams and tableaus

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -360,3 +360,14 @@
     (cons-stream (- s2 (/ (square (- s2 s1))
                           (+ s0 (* -2 s1) s2)))
                  (euler-transform (stream-cdr s)))))
+
+(display-stream (euler-transform pi-stream))
+
+
+(define (make-tableau transform s)
+  (cons-stream s (make-tableau transform (transform s))))
+
+(define (accelerated-sequence transform s)
+  (stream-map stream-car (make-tableau transform s))))
+
+(stream-ref (accelerated-sequence euler-transform pi-stream) 0)
