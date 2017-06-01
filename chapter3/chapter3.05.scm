c5b0efb81a58163952b93e2b51adee92731d0012
Grigory Bordyugov <gregbg@gmail.com>
2017-06-01 09:58:43 +0200

    exercise 3.65 done

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -398,3 +398,17 @@
 (sqrt-l 2.0 0.001)
 
 (sqrt-l 256 0.1)
+
+
+;;
+;; exercise 3.65
+;;
+
+(define (ln2-summands n)
+  (cons-stream (/ 1.0 n)
+               (stream-map - (ln2-summands (+ n 1)))))
+
+(define ln2-stream
+  (partial-sums (ln2-summands 1)))
+
+(stream-ref (accelerated-sequence euler-transform ln2-stream) 0)
