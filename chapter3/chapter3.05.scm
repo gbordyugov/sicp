4d211dccf3ac7f811326dfb13e2d029f69977471
Grigory Bordyugov <gregbg@gmail.com>
2017-06-06 11:12:08 +0200

    started Streams as signals

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -624,3 +624,16 @@
                      (= (square-weight (cadr  x))
                         (square-weight (caddr x)))))
                  triples-of-integers))
+
+
+
+;;
+;; Streams as signals
+;;
+
+(define (integral integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+                 (add-streams (scale-stream integrand dt)
+                              int)))
+  int)
