eab2740a5f884e63388f9556fc320c1ee39f893f
Grigory Bordyugov <gregbg@gmail.com>
2017-06-07 09:15:14 +0200

    exercise 3.74 done

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -652,4 +652,25 @@
     (add-streams int (scale-stream int R)))
   go)
 
+
+;;
+;; exercise 3.74
+;;
+
+(define (make-zero-crossings input-stream last-value)
+  (cons-stream
+    (sign-change-detector
+      (stream-car input-stream)
+      last-value)
+    (make-zero-crossings
+      (stream-cdr input-stream)
+      (stream-car input-stream))))
+
+(define zero-crossings
+  (make-zero-crossings sense-data 0))
+
+(define zero-crossings
+  (stream-map sign-change-detector
+              sense-data
+              (stream-cdr sense-data)))
                                 
