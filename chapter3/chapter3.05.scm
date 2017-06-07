fd506ac8e3cfc685aa89e275f6decf7b3b6914cd
Grigory Bordyugov <gregbg@gmail.com>
2017-06-07 09:57:44 +0200

    exercise 3.78 done

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -747,3 +747,16 @@
                   (+ (* dt (stream-car integrand))
                      initial-value)
                   dt)))))
+
+
+;;
+;; exercise 3.78
+;;
+
+(define (solve-2nd a b dt y0 dy0)
+  (define y   (integral (delay dy ) y0 dt))
+  (define dy  (integral (delay ddy) dy0 dt))
+  (define ddy (add-streams
+                (scale-stream dy a)
+                (scale-stream  y b)))
+  y)
