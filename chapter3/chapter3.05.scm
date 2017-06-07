b0eebd4634d7eacee0bc4693901742525947c621
Grigory Bordyugov <gregbg@gmail.com>
2017-06-07 09:58:46 +0200

    exercise 3.79 done

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -760,3 +760,14 @@
                 (scale-stream dy a)
                 (scale-stream  y b)))
   y)
+
+
+;;
+;; exercise 3.79
+;;
+
+(define (solve-2nd f dt y0 dy0)
+  (define y   (integral (delay dy ) y0 dt))
+  (define dy  (integral (delay ddy) dy0 dt))
+  (define ddy (stream-map f dy y))
+  y)
