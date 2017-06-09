0e542d6fb0fa4742dee0615eec53eb734283a72b
Grigory Bordyugov <gregbg@gmail.com>
2017-06-09 09:46:43 +0200

    3.5.5 example done

--- chapter3/chapter3.05.scm
+++ chapter3/chapter3.05.scm
@@ -835,15 +835,37 @@
 ;; 3.5.5 Modularity of Functional Programs and Modularity of Objects
 ;;
 
+(define random-init 1)
 
-;;
-;; exercise 3.81
-;;
+(define (rand-update i)
+  (random (* 1000 1000)))
 
-(define (process-requests requests)
-  (if (stream-null? requests)
-    the-empty-stream
-    ((let ((request (stream-car requests)))
-       (cond ((eq? request 'generate) (do-smth))
-             ((eq? request 'reset)    (do-smth))
-             (else (error "Unknown request: PROCESS-REQUESTS")))))))
+(define random-numbers
+  (cons-stream
+    random-init
+    (stream-map rand-update random-numbers)))
+
+
+(define (map-successive-pairs f s)
+  (cons-stream
+    (f (stream-car s) (stream-car (stream-cdr s)))
+    (map-successive-pairs f (stream-cdr (stream-cdr s)))))
+
+(define cesaro-stream
+  (map-successive-pairs
+    (lambda (r1 r2) (= (gcd r1 r2) 1))
+    random-numbers))
+
+(define (monte-carlo experiment-stream passed failed)
+  (define (next passed failed)
+    (cons-stream
+      (/ passed (+ passed failed))
+      (monte-carlo
+        (stream-cdr experiment-stream) passed failed)))
+  (if (stream-car experiment-stream)
+    (next (+ passed 1) failed)
+    (next passed (+ failed 1))))
+
+(define pi
+  (stream-map (lambda (p) (sqrt (/ 6 p)))
+              (monte-carlo cesaro-stream 0 0)))
