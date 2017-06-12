5d25d4ca5290f0b627abfdfdf5abc907c3175f82
Grigory Bordyugov <gregbg@gmail.com>
2017-06-12 09:40:22 +0200

    eval and apply

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -1,3 +1,62 @@
 ;;
 ;; Chapter 4 Metalinguistic Abstraction
 ;;
+
+;;
+;; evaluator: a procedure that, when applied to an expression,
+;; performs the actions required to evaluate that expression
+;;
+
+;;
+;; the most fundamental idea in programming:
+;;
+;; The evaluator, which determines the meaning of expressions in a
+;; programming language, is just another program
+;;
+
+;;
+;; (the real ;) programmers are designers of languages, rather than
+;; only users of languages designed by others
+;;
+
+;;
+;; 4.1 The Metacircular Evaluator
+;;
+
+
+;;
+;; 4.1.1 The core of the Evaluator
+;;
+
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp)       (lookup-variable-value exp env))
+        ((quoted? exp)         (text-of-quotation exp))
+        ((assignment? exp)     (eval-assignment exp env))
+        ((definition? exp)     (eval-definition exp env))
+        ((if? exp)             (eval-if exp env))
+        ((lambda? exp)         (make-procedure (lambda-parameters exp)
+                                               (lambda-body exp)
+                                               env))
+        ((begin? exp)
+         (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        (else
+          (error "Unknown expression-type: EVAL" exp))))
+
+(define (apply procedure arguments)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure procedure arguments))
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+             arguments
+             (procedure-environment procedure))))
+        (else
+          (error "Unknown procedure type: APPLY" procedure))))
