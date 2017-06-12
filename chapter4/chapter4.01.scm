18a05a240bd58619e35e649e36ecbfa7445b539d
Grigory Bordyugov <gregbg@gmail.com>
2017-06-12 09:57:15 +0200

    exercise 4.1 done

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -95,3 +95,22 @@
                     (eval (definition-value exp) env)
                     env)
   'ok)
+
+
+;;
+;; exercise 4.1
+;;
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+    '()
+    (let ((first (eval (first-operand exps) env)))
+      (cons first
+            (list-of-values (rest-operands exps) env)))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+    '()
+    (let ((rest (list-of-values (rest-operands exps) env)))
+      (cons (eval (first-operand exps) env)
+            rest))))
