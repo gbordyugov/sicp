f8bbd88187d606652902a35ac65d9f21677c8fbe
Grigory Bordyugov <gregbg@gmail.com>
2017-06-13 09:50:29 +0200

    Derived expressions

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -251,3 +251,35 @@
   (car ops))
 (define (rest-operands ops)
   (cdr ops))
+
+
+;;
+;; Derived expressions
+;;
+
+(define (cond? exp)
+  (tagged-list? exp 'cond))
+(define (cond-clauses exp)
+  (cdr exp))
+(define (cond-else-clause? clause)
+  (eq? (cond-predicate clause) 'else))
+(define (cond-predicate clause)
+  (car clause))
+(define (cond-actions clause)
+  (cdr clause))
+(define (cond->if exp)
+  (expand-clauses (cond-clauses exp)))
+
+(define (expand-clauses clauses)
+  (if (null? clauses)
+    'false
+    (let ((first (car clauses))
+          (rest  (cdr clauses)))
+      (if (cond-else-clause? first)
+        (if (null? rest)
+          (sequence->exp (cond-actions first))
+          (error "ELSE clause isn't last: COND->IF"
+                 clauses))
+        (make-if (cond-predicate first)
+                 (sequence->exp (cond-actions first))
+                 (expand-clauses rest))))))
