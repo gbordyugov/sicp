12149163af5093e267abcc0f25c81b496f7c85f3
Grigory Bordyugov <gregbg@gmail.com>
2017-06-28 10:11:10 +0200

    driver loop done

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -807,9 +807,9 @@
     (define (scan vars vals)
       (cond ((null? vars) (add-binding-to-frame! var val frame))
             ((eq? var (car vars)) (set-car! vals val))
-            (else (scan (cdr vars) (cdr vals))))))
-  (scan (frame-variables frame)
-        (frame-values    frame)))
+            (else (scan (cdr vars) (cdr vals)))))
+    (scan (frame-variables frame)
+          (frame-values    frame))))
 
 
 ;;
@@ -933,8 +933,7 @@
     (define-variable! 'false false initial-env)
     initial-env))
 
-(define the-global-environment (setup-environment))
-
+;; (define the-global-environment (setup-environment))
 
 (define (primitive-procedure? proc)
   (tagged-list? proc 'primitive))
@@ -948,6 +947,7 @@
         (list 'cons  cons)
         (list 'null? null?)
         ;; ... more primitives))
+        ))
 
 (define (primitive-procedure-names)
   (map car primitive-procedures))
@@ -960,3 +960,32 @@
 (define (apply-primitive-procedure proc args)
   (apply-in-underlyng-scheme
     (primitive-implementation proc) args))
+
+(define  input-prompt ";;; M-Eval input:")
+(define output-prompt ";;; M-Eval value:")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+(define (prompt-for-input string)
+  (newline) (newline) (display string) (newline))
+
+(define (announce-output string)
+  (newline) (display string) (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+    (display (list 'compound-procedure
+                   (procedure-parameters object)
+                   (procedure-body       object)
+                   '<procedure-env>))
+    (display object)))
+
+(define the-global-environment (setup-environment))
+
+;; (driver-loop)
