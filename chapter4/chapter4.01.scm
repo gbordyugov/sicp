8b7d79d4f07aef73f4d914b7448d83fb0f99fd1c
Grigory Bordyugov <gregbg@gmail.com>
2017-06-28 09:50:57 +0200

    applying primitive procedures

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -934,3 +934,29 @@
     initial-env))
 
 (define the-global-environment (setup-environment))
+
+
+(define (primitive-procedure? proc)
+  (tagged-list? proc 'primitive))
+
+(define (primitive-implementation proc)
+  (cadr proc))
+
+(define primitive-procedures
+  (list (list 'car   car)
+        (list 'cdr   cdr)
+        (list 'cons  cons)
+        (list 'null? null?)
+        ;; ... more primitives))
+
+(define (primitive-procedure-names)
+  (map car primitive-procedures))
+
+(define (primitive-procedure-objects)
+  (map (lambda (proc)
+         (list 'primitive (cadr proc)))
+       primitive-procedures))
+
+(define (apply-primitive-procedure proc args)
+  (apply-in-underlyng-scheme
+    (primitive-implementation proc) args))
