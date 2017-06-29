9cad0fd8644cc57fb10e87355c80058d40a136a9
Grigory Bordyugov <gregbg@gmail.com>
2017-06-29 09:05:17 +0200

    exercise 4.16 (a) done

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -1012,3 +1012,29 @@
 ;;
 ;; 4.1.6 Internal Definitions
 ;;
+
+
+;;
+;; exercise 4.16
+;;
+
+;;
+;; a.
+;;
+
+(define (lookup-variable-value var env)
+  (define (env-loop env)
+    (define (scan vars vals)
+      (cond ((null? vars) (env-loop (enclosing-environment env)))
+            ((eq? var (car vars))
+             (let ((val (car vals)))
+               (if (eq? val *unassigned*)
+                 (error "an unassigned variable" var)
+                 val)))
+            (else (scan (cdr vars) (cdr vals)))))
+    (if (eq? env the-empty-environment)
+      (error "Unbound variable" var)
+      (let ((frame (first-frame env)))
+        (scan (frame-variables frame)
+              (frame-values    frame)))))
+    (env-loop env))
