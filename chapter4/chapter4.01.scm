86f205941021cea4ba37c3485145559b9cd1d203
Grigory Bordyugov <gregbg@gmail.com>
2017-07-04 09:56:00 +0200

    exercise 4.21 done

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -1231,3 +1231,23 @@
                      (+ (fi fi (- k 1))
                         (fi fi (- k 2)))))))
  7)
+
+
+;;
+;; (b)
+;;
+
+(define (f x)
+  ((lambda (even? odd?) (even? even? odd? x))
+   (lambda (ev? od? n)
+     (if (= n 0)
+       #t
+       (od? ev? od? (- n 1))))
+   (lambda (ev? od? n)
+     (if (= n 0)
+       #f
+       (ev? ev? od? (- n 1))))))
+
+(f 1)
+
+(f 2)
