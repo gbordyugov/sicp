d4f6ba24373fa577a13f0645a20c25f9c4e5e6d3
Grigory Bordyugov <gregbg@gmail.com>
2017-07-04 10:27:32 +0200

    main text of 4.1.7 done

--- chapter4/chapter4.01.scm
+++ chapter4/chapter4.01.scm
@@ -1334,3 +1334,39 @@
         (bproc (analyze-sequence (lambda-body exp))))
     (lambda (env)
       (make-procedure vars bproc env))))
+
+
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env)
+      (proc1 env)
+      (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+      first-proc
+      (loop (sequentially first-proc (car rest-procs))
+            (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+      (error "Empty sequence: ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+
+(define (analyze-application exp)
+  (let ((fproc  (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (execute-application
+        (fproc env)
+        (map (lambda (aproc) (aproc env)) aprocs)))))
+
+(define (execute-application proc args)
+  (cond ((primitive-procedure? proc)
+         (apply-primitive-procedure proc args))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))))
+        (else
+          (error "Unknown procedure type: EXECUTE-APPLICATION" proc))))
