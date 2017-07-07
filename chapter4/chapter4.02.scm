59078aef666b2c3e1b1142d9423568733fe197c7
Grigory Bordyugov <gregbg@gmail.com>
2017-07-07 09:13:32 +0200

    started exercise 4.26

--- chapter4/chapter4.02.scm
+++ chapter4/chapter4.02.scm
@@ -9,3 +9,20 @@
 ;;
 ;; this would result in an infinite loop
 ;;
+
+
+;;
+;; exercise 4.26
+;;
+
+(define (unless? exp)
+  (tagged-list exp 'unless))
+
+(define (unless-condition exp)
+  (cadr exp))
+
+(define (unless-condition-usual-value exp)
+  (caddr exp))
+
+(define (unless-exceptional-value exp)
+  (cDddr exp))
