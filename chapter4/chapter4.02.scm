e5ac17fcc31a2575bc1c00656de95f5490ab5a3d
Grigory Bordyugov <gregbg@gmail.com>
2017-07-07 10:13:28 +0200

    modifying evaluator to make it lazy

--- chapter4/chapter4.02.scm
+++ chapter4/chapter4.02.scm
@@ -55,3 +55,46 @@
 ;;  - when it is the value of an operator that is about to be applied
 ;;  as a procedure
 ;;
+
+;;
+;; modified procedure application
+;;
+;; ((application? exp)
+;;  (apply (actual-value (operator exp) env)
+;;         (operands exp)
+;;         env))
+
+
+(define (actual-value exp env)
+  (force-it (eval exp env)))
+
+(define (apply. procedure arguments env)
+  (cond ((primitive-procedure? procedure)
+         (apply-primitive-procedure
+           procedure
+           (list-of-arg-values arguments env))) ;; changed
+        ((compound-procedure? procedure)
+         (eval-sequence
+           (procedure-body procedure)
+           (extend-environment
+             (procedure-parameters procedure)
+             (list-of-delayed-args arguments env)   ;; changed
+             (procedure-environment procedure))))
+        (else (error "Unknown procedure type: APPLY" procedure))))
+
+(define (list-of-arg-values exps env)
+  (if (no-operands? exps)
+    '()
+    (cons (actual-value (first-operand exps) env)
+          (list-of-arg-values (rest-operands exps) env))))
+
+(define (list-of-delayed-args exps env)
+  (if (no-operands? exps)
+    '()
+    (cons (delay-it (first-operand exps) env)
+          (list-of-delayed-args (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (actual-value (if-predicateexp) env))
+    (eval (if-consequent  exp) env)
+    (eval (if-alternative exp) env)))
