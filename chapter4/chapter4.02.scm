700fb1721541475ca3d4b535a18d3ee1af09dc23
Grigory Bordyugov <gregbg@gmail.com>
2017-07-07 10:43:04 +0200

    introduced lazy eval into evaluator, the inner workins of thunks to come

--- chapter4/chapter4.02.scm
+++ chapter4/chapter4.02.scm
@@ -98,3 +98,19 @@
   (if (true? (actual-value (if-predicateexp) env))
     (eval (if-consequent  exp) env)
     (eval (if-alternative exp) env)))
+
+(define  input-prompt ";;; L-Eval input:")
+(define output-prompt ";;; L-Eval value")
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input-prompt (read)))
+    (let ((output (actual-value input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+(define the-global-environment (setup-environment))
+
+;; (driver-loop)
+      
