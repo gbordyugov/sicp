400e9909c7f0db1adc7d88dbe5c723b37e420664
Grigory Bordyugov <gregbg@gmail.com>
2017-07-11 10:12:33 +0200

    exercise 4.36 (a) done

--- chapter4/chapter4.03.scm
+++ chapter4/chapter4.03.scm
@@ -10,3 +10,15 @@
 (define (an-integer-between low hi)
   (require (<= low hi))
   (amb low (an-integer-between (+ 1 low) hi)))
+
+
+;;
+;; exercise 4.36
+;;
+
+;;
+;; (a)
+;;
+;; explanation of the non-adequacy: nested an-integer-starting-from's
+;; would try to go through all values of k before changing i and j
+;;
