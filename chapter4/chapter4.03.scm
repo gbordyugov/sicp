c9babaecaf943ea7481278d81d0bccea1704f984
Grigory Bordyugov <gregbg@gmail.com>
2017-07-11 10:06:35 +0200

    exercise 4.35 done

--- chapter4/chapter4.03.scm
+++ chapter4/chapter4.03.scm
@@ -1,3 +1,12 @@
 ;;
 ;; 4.3 Variations on a Scheme - Nondeterministic Computing
 ;;
+
+
+;;
+;; exercise 4.35
+;;
+
+(define (an-integer-between low hi)
+  (require (<= low hi))
+  (amb low (an-integer-between (+ 1 low) hi)))
