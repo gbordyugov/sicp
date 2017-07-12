72e5e0ed29a273909bc2f6bd06d9169b4ae225e7
Grigory Bordyugov <gregbg@gmail.com>
2017-07-12 08:26:59 +0200

    exercise 4.36 done

--- chapter4/chapter4.03.scm
+++ chapter4/chapter4.03.scm
@@ -22,3 +22,23 @@
 ;; explanation of the non-adequacy: nested an-integer-starting-from's
 ;; would try to go through all values of k before changing i and j
 ;;
+
+;;
+;; (b)
+;;
+;; note that the desired (i, j, k) are side lengths of a triangle and
+;; hence k <= i + j
+;;
+;; hence the solution
+;;
+
+
+(define (a-pythagorean-triple)
+  (let ((i (an-integer-starting-from 1)))
+    (let ((j (an-integer-between 1 i)))
+      (let ((k (an-integer-between 1 (+ i j))))
+        (require (= (* k k) (+ (* i i) (* j j))))
+        (list i j k)))))
+
+
+
