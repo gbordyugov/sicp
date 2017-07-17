58da3290db224772cf3cf21a35910dc93af560d5
Grigory Bordyugov <gregbg@gmail.com>
2017-07-17 09:58:19 +0200

    added analyze to simple-interpreter.scm

--- chapter4/simple-interpreter.scm
+++ chapter4/simple-interpreter.scm
@@ -1023,126 +1023,130 @@
 ;; (f 2)
 ;; 
 ;; 
-;; ;;
-;; ;; 4.1.7 Separating Syntactic Analysis from Execution
-;; ;;
-;; 
-;; ;;
-;; ;; analyze takes expression and returns a procedure, the execution
-;; ;; procedure, that encapsulates the work to be done in executing the
-;; ;; analyzed expression
-;; ;;
-;; ;; the execution procedure takes an environment as its argument and
-;; ;; completes the evaluation
-;; ;;
-;; ;; This saves work because `analyze` will be called only once on an
-;; ;; expression, while the execution procedure may be called many times.
-;; ;; (I don't quite understant it yet. Are they saying that partially
-;; ;; applying eval. to exp will be done once and then the result will be
-;; ;; applied to environment many times?)
-;; ;;
-;; 
-;; (define (geval exp env)
-;;   ((analyze exp) env))
-;; 
-;; (define (analyze exp)
-;;   (cond ((self-evaluating? exp) (analyze-self-evaluating exp))
-;;         ((quoted?          exp) (analyze-quoted          exp))
-;;         ((variable?        exp) (analyze-variable        exp))
-;;         ((assignment?      exp) (analyze-assignment      exp))
-;;         ((definition?      exp) (analyze-definition      exp))
-;;         ((if?              exp) (analyze-if              exp))
-;;         ((lambda?          exp) (analyze-lambda          exp))
-;;         ((begin?           exp) (analyze-sequence        (begin-actions exp)))
-;;         ((cond?            exp) (analyze                 (cond->if exp)))
-;;         ((application?     exp) (analyze-application     exp))
-;;         (else (error "Unknown expression type: ANALYZE" exp))))
-;; 
-;; 
-;; (define (analyze-self-evaluating exp)
-;;   (lambda (env) exp))
-;; 
-;; 
-;; (define (analyze-quoted exp)
-;;   (let ((qval (text-of-quotation exp)))
-;;     (lambda (env) qval)))
-;; 
-;; 
-;; (define (analyze-variable exp)
-;;   (lambda (env)
-;;     (lookup-variable-value exp env)))
-;; 
-;; 
-;; (define (analyze-assignment exp)
-;;   (let ((var   (assignment-variable exp))
-;;         (vproc (analyze (assignment-value exp))))
-;;     (lambda (env)
-;;       (set-variable-value! var (vproc env) env)
-;;       'ok)))
-;; 
-;; 
-;; (define (analyze-definition exp)
-;;   (let ((var   (definition-variable exp))
-;;         (vproc (analyze (definition-value exp))))
-;;     (lambda (env)
-;;       (define-variable! var (vproc env) env)
-;;       'ok)))
-;; 
-;; 
-;; (define (analyze-if exp)
-;;   (let ((pproc (analyze (if-predicate   exp)))
-;;         (cproc (analyze (if-consequent  exp)))
-;;         (aproc (analyze (if-alternative exp))))
-;;     (lambda (env)
-;;       (if (true? (pproc env))
-;;         (cproc env)
-;;         (aproc env)))))
-;; 
-;; 
-;; (define (analyze-lambda exp)
-;;   (let ((vars  (lambda-parameters exp))
-;;         (bproc (analyze-sequence (lambda-body exp))))
-;;     (lambda (env)
-;;       (make-procedure vars bproc env))))
-;; 
-;; 
-;; (define (analyze-sequence exps)
-;;   (define (sequentially proc1 proc2)
-;;     (lambda (env)
-;;       (proc1 env)
-;;       (proc2 env)))
-;;   (define (loop first-proc rest-procs)
-;;     (if (null? rest-procs)
-;;       first-proc
-;;       (loop (sequentially first-proc (car rest-procs))
-;;             (cdr rest-procs))))
-;;   (let ((procs (map analyze exps)))
-;;     (if (null? procs)
-;;       (error "Empty sequence: ANALYZE"))
-;;     (loop (car procs) (cdr procs))))
-;; 
-;; 
-;; (define (analyze-application exp)
-;;   (let ((fproc  (analyze (operator exp)))
-;;         (aprocs (map analyze (operands exp))))
-;;     (lambda (env)
-;;       (execute-application
-;;         (fproc env)
-;;         (map (lambda (aproc) (aproc env)) aprocs)))))
-;; 
-;; (define (execute-application proc args)
-;;   (cond ((primitive-procedure? proc)
-;;          (apply-primitive-procedure proc args))
-;;         ((compound-procedure? proc)
-;;          ((procedure-body proc)
-;;           (extend-environment (procedure-parameters proc)
-;;                               args
-;;                               (procedure-environment proc))))
-;;         (else
-;;           (error "Unknown procedure type: EXECUTE-APPLICATION" proc))))
-;; 
-;; 
-;; 
+;;
+;; 4.1.7 Separating Syntactic Analysis from Execution
+;;
+
+;;
+;; analyze takes expression and returns a procedure, the execution
+;; procedure, that encapsulates the work to be done in executing the
+;; analyzed expression
+;;
+;; the execution procedure takes an environment as its argument and
+;; completes the evaluation
+;;
+;; This saves work because `analyze` will be called only once on an
+;; expression, while the execution procedure may be called many times.
+;; (I don't quite understant it yet. Are they saying that partially
+;; applying eval. to exp will be done once and then the result will be
+;; applied to environment many times?)
+;;
+
+(define (eval. exp env)
+  ((analyze exp) env))
+
+(define (analyze exp)
+  (cond ((self-evaluating? exp) (analyze-self-evaluating   exp))
+        ((quoted?          exp) (analyze-quoted            exp))
+        ((variable?        exp) (analyze-variable          exp))
+        ((assignment?      exp) (analyze-assignment        exp))
+        ((definition?      exp) (analyze-definition        exp))
+        ((let?             exp) (analyze
+                                  (let->application        exp)))
+        ((if?              exp) (analyze-if                exp))
+        ((lambda?          exp) (analyze-lambda            exp))
+        ((begin?           exp) (analyze-sequence 
+                                  (begin-actions           exp)))
+        ((cond?            exp) (analyze
+                                  (cond->if                exp)))
+        ((application?     exp) (analyze-application       exp))
+        (else (error "Unknown expression type: ANALYZE" exp))))
+
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+
+(define (analyze-quoted exp)
+  (let ((qval (text-of-quotation exp)))
+    (lambda (env) qval)))
+
+
+(define (analyze-variable exp)
+  (lambda (env)
+    (lookup-variable-value exp env)))
+
+
+(define (analyze-assignment exp)
+  (let ((var   (assignment-variable exp))
+        (vproc (analyze (assignment-value exp))))
+    (lambda (env)
+      (set-variable-value! var (vproc env) env)
+      'ok)))
+
+
+(define (analyze-definition exp)
+  (let ((var   (definition-variable exp))
+        (vproc (analyze (definition-value exp))))
+    (lambda (env)
+      (define-variable! var (vproc env) env)
+      'ok)))
+
+
+(define (analyze-if exp)
+  (let ((pproc (analyze (if-predicate   exp)))
+        (cproc (analyze (if-consequent  exp)))
+        (aproc (analyze (if-alternative exp))))
+    (lambda (env)
+      (if (true? (pproc env))
+        (cproc env)
+        (aproc env)))))
+
+
+(define (analyze-lambda exp)
+  (let ((vars  (lambda-parameters exp))
+        (bproc (analyze-sequence (lambda-body exp))))
+    (lambda (env)
+      (make-procedure vars bproc env))))
+
+
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env)
+      (proc1 env)
+      (proc2 env)))
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+      first-proc
+      (loop (sequentially first-proc (car rest-procs))
+            (cdr rest-procs))))
+  (let ((procs (map analyze exps)))
+    (if (null? procs)
+      (error "Empty sequence: ANALYZE"))
+    (loop (car procs) (cdr procs))))
+
+
+(define (analyze-application exp)
+  (let ((fproc  (analyze (operator exp)))
+        (aprocs (map analyze (operands exp))))
+    (lambda (env)
+      (execute-application
+        (fproc env)
+        (map (lambda (aproc) (aproc env)) aprocs)))))
+
+(define (execute-application proc args)
+  (cond ((primitive-procedure? proc)
+         (apply-primitive-procedure proc args))
+        ((compound-procedure? proc)
+         ((procedure-body proc)
+          (extend-environment (procedure-parameters proc)
+                              args
+                              (procedure-environment proc))))
+        (else
+          (error "Unknown procedure type: EXECUTE-APPLICATION" proc))))
+
+
+
 ;; ;;
 ;; ;; exercise 4.22
 ;; ;;
