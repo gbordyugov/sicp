f48235de4e0d856c8bed286ce94348dc76328ffd
Grigory Bordyugov <gregbg@gmail.com>
2017-08-11 08:56:38 +0200

    before exercise 4.45

--- chapter4/chapter4.03.scm
+++ chapter4/chapter4.03.scm
@@ -510,3 +510,40 @@
   (set! *unparsed* input)
   (let ((sent (parse-sentence)))
     (require (null? *unparsed*)) sent))
+
+;;
+;; extension
+;;
+
+(define prepositions '(prep for to in by with))
+
+(define (parse-prepositional-phrase)
+  (list 'prep-phrase
+        (parse-word prepositions)
+        (parse-noun-phrase)))
+
+(define (parse-sentence)
+  (list 'sentence (parse-noun-phrase) (parse-verb-phrase)))
+
+(define (parse-verb-phrase)
+  (define (maybe-extend verb-phrase)
+    (amb verb-phrase
+         (maybe-extend (list 'verb-phrase verb-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-word verbs)))
+
+(define (parse-simple-noun-phrase)
+  (list 'simple-noun-phrase
+        (parse-word articles)
+        (parse-word nouns)))
+
+(define (parse-noun-phrase)
+  (define (maybe-extend noun-phrase)
+    (amb noun-phrase
+         (maybe-extend (list 'noun-phrase noun-phrase
+                             (parse-prepositional-phrase)))))
+  (maybe-extend (parse-simple-noun-phrase)))
+
+(parse '(the student with the cat sleeps in the class))
+
+(parse '(the professor lectures to the student with the cat))
