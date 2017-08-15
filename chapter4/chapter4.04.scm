1b60deac1e7653e734d5e67f88278efb7e2c97c0
Grigory Bordyugov <gregbg@gmail.com>
2017-08-15 09:43:35 +0200

    exercise 4.55 done

--- chapter4/chapter4.04.scm
+++ chapter4/chapter4.04.scm
@@ -7,3 +7,25 @@
 ;;
 ;; use smth similar for our KPI queries?
 ;;
+
+;;
+;; exercise 4.55
+;;
+
+;;
+;; 1. all people supervised by Ben Bitdiddle
+;;
+
+(supervisor ?name (Bitdiddle Ben))
+
+;;
+;; 2. the names and jobs of all people in the accounting division
+;;
+
+(job ?name (accounting . ?job))
+
+;;
+;; 3. the names and addresses of all ppl who live in Slumerville
+;;
+
+(address ?name (Slummerville . ?address))
