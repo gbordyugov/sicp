80a1e47ab82e01dd44f2216a5aaf1824ee064cc4
Grigory Bordyugov <gregbg@gmail.com>
2017-08-17 11:39:20 +0200

    reading 4.4.2

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -1,3 +1,22 @@
 ;;
 ;; 4.4.2 How the Query System Works
 ;;
+
+;;
+;; two possibilities: either amb-similar design or stream of frames
+;;
+
+;;
+;; Two ideas:
+;;
+;; 1. Pattern matching
+;;
+;; 2. Unification
+;;
+
+
+;;
+;; Pattern matching
+;;
+;; ((a b) c (a b)) matches (?x c ?x) with ?x bound to (a b)
+;;
