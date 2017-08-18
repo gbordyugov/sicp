f904ad8bbf15e176f757d72ff121602389b90049
Grigory Bordyugov <gregbg@gmail.com>
2017-08-18 09:33:38 +0200

    query system: pattern matching

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -20,3 +20,16 @@
 ;;
 ;; ((a b) c (a b)) matches (?x c ?x) with ?x bound to (a b)
 ;;
+;; Input of the pattern matcher:
+;;  - 1. a pattern
+;;  - 2. a piece of data
+;;  - 3. a frame possibly specifying bindings (from previous pattern
+;;  matching possibly
+;;
+;; (?x ?y ?x) will match (a b a) given an empty frame
+;;
+;; (?x ?y ?x) will fail to match (a b a) given a frame with ?b bound to c
+;;
+;; Pattern matching is the only mechanism needed to process simple
+;; queries
+;;
