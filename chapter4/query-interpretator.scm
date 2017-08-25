82e607849bc4f616ee96606bb2a8179db0ddf838
Grigory Bordyugov <gregbg@gmail.com>
2017-08-25 10:04:39 +0200

    query system: the driver loop

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -266,3 +266,37 @@
 ;;
 ;; Remark 78 (about (not (baseball-fan (Bitdiddle Ben)))) is awesome
 ;;
+
+
+;;
+;; 4.4.4 Implementing the Query System
+;;
+
+
+;;
+;; The Driver Loop and Insatantiation
+;;
+
+(define  input-prompt ";;; Query input:")
+(define output-prompt ";;; Query results:")
+
+(define (query-driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((q (query-syntax-process (read))))
+    (cond ((assertion-to-be-added? q)
+           (add-rule-or-assertion! (add-assertion-body q))
+           (newline)
+           (display "Assertion added to data base.")
+           (query-driver-loop))
+          (else
+            (newline)
+            (display output-prompt)
+            (display-stream
+              (stream-map
+                (lambda (frame)
+                  (instantiate
+                    q frame
+                    (lambda (v f)
+                      (contract-question-mark v))))
+                (qeval q (singleton-stream '()))))
+            (query-driver-loop)))))
