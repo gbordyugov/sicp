9bc1fdb66b77b3b66d453cf041e7d8de4c58575e
Grigory Bordyugov <gregbg@gmail.com>
2017-08-25 11:10:28 +0200

    query system: instantiate

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -300,3 +300,15 @@
                       (contract-question-mark v))))
                 (qeval q (singleton-stream '()))))
             (query-driver-loop)))))
+
+(define (instantiate exp frame unbound-var-handler)
+  (define (copy exp)
+    (cond ((var? exp)
+           (let ((binding (binding-in-frame exp frame)))
+             (if binding
+               (copy (binding-value binding))
+               (unbound-var-handler exp frame))))
+          ((pair? exp)
+           (cons (copy (car exp)) (copy (cdr exp))))
+          (else exp)))
+  (copy exp))
