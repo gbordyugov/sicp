8b34a1375470bfe2051677d4075afbdcaf90a4ac
Grigory Bordyugov <gregbg@gmail.com>
2017-08-28 09:32:01 +0200

    query system: lisp-value

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -113,3 +113,18 @@
         the-empty-stream))
     frame-stream))
 ;; (put 'not 'qeval negate)
+
+
+(define (lisp-value call frame-stream)
+  (stream-flatmap
+    (lambda (frame)
+      (if (execute
+            (instantiate
+              call
+              frame
+              (lambda (v f)
+                (error "Unknown pat var: LISP-VALUE" v))))
+        (singletone-stream frame)
+        the-empty-stream))
+    frame-stream))
+;; (put 'lisp-value 'qeval lisp-value)
