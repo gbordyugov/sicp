c74d079c61acb7dd4dcebe31f5fc0af3ea5306ed
Grigory Bordyugov <gregbg@gmail.com>
2017-08-28 09:21:53 +0200

    query system: disjoin

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -356,3 +356,17 @@
              (qeval (first-conjunct conjuncts) frame-stream))))
 
 ;; (put 'and 'qeval conjoin)
+
+
+;;
+;; disjoin takes care of `or` queries
+;;
+
+(define (disjoin disjuncts frame-stream)
+  (if (empty-disjunction? disjuncts)
+    the-empty-stream
+    (interleave-delayed
+      (qeval (first-disjunct disjuncts) frame-stream)
+      (delay (disjoin (rest-disjuncts disjuncts) frame-stream)))))
+
+;; (put 'or 'qeval disjoin
