3c6a40939a1e0f0efac687fb0ff75c214b713952
Grigory Bordyugov <gregbg@gmail.com>
2017-09-20 09:59:11 +0200

    4.4.4.6 Stream Operations

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -159,3 +159,34 @@
 ;;
 ;; continue on 4.4.4.3 Finding Assertions by Pattern Matching
 ;;
+
+
+;;
+;; 4.4.4.6 Stream Operations
+;;
+
+
+(define (stream-append-delayed s1 delayed-s2)
+  (if (stream-null? s1)
+    (force delayed-s2)
+    (cons-stream (stream-car s1) (stream-append-delayed (stream-cdr s1)
+                                                        delayed-s2))))
+
+(define (interleave-delayed s1 delayed-s2)
+  (if (stream-null? s1)
+    (force delayed-s2)
+    (cons-stream (stream-car s1)
+                 (interleave-delayed (force delayed-s2)
+                                     (delay (stream-cdr s1))))))
+
+(define (stream-flatmap proc s)
+  (flatten-stream (stream-map proc s)))
+
+(define (flatten-stream stream)
+  (if (stream-null? stream)
+    the-empty-stream
+    (interleave-delayed (stream-car stream)
+                        (delay (flatten-stream (stream-cdr stream))))))
+
+(define (singleton-stream x)
+  (cons-stream x the-empty-stream))
