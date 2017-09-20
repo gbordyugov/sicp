e334371e536a65fe9bddfeed39e3a8d6b0138913
Grigory Bordyugov <gregbg@gmail.com>
2017-09-20 10:03:10 +0200

    4.4.4.8 Frames and Bindings

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -190,3 +190,20 @@
 
 (define (singleton-stream x)
   (cons-stream x the-empty-stream))
+
+
+;;
+;; 4.4.4.8 Frames and Bindings
+;;
+
+(define (make-binding variable value)
+  (cons variable value))
+
+(define (binding-variable binding) (car binding))
+(define (binding-value    binding) (cdr binding))
+
+(define (binding-in-frame variable frame)
+  (assoc variable frame))
+
+(define (extend variable value frame)
+  (cons (make-binding variable value) frame))
