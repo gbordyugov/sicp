5664c16e8448574dc900717783d6b55fbb3ebd4c
Grigory Bordyugov <gregbg@gmail.com>
2017-09-22 10:08:44 +0200

    started 4.4.4.4 Rules and Unification

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -162,6 +162,25 @@
 
 
 ;;
+;; 4.4.4.4 Rules and Unification
+;;
+
+(define (apply-rules pattern frame)
+  (stream-flatmap (lambda (rule)
+                    (apply-a-rule rule pattern frame))
+                  (fetch-rules pattern frame)))
+
+(define (apply-a-rule rule query-pattern query-frame)
+  (let ((clean-rule (rename-variables-in rule)))
+    (let ((unify-result (unify-match query-pattern
+                                     (conclusion clean-rule)
+                                     query-frame)))
+      (if (eq? unify-result 'failed)
+        the-empty-stream
+        (qeval (rule-body clean-rule)
+               (singleton-stream unify-result))))))
+
+;;
 ;; 4.4.4.5 Maintaining the Data Base
 ;;
 
