1fde6bd5db73075f5bb396a1529fe210ff9f7749
Grigory Bordyugov <gregbg@gmail.com>
2017-09-25 09:58:49 +0200

    4.4.4.4 Rules and Unification

--- chapter4/query-interpretator.scm
+++ chapter4/query-interpretator.scm
@@ -170,6 +170,7 @@
                     (apply-a-rule rule pattern frame))
                   (fetch-rules pattern frame)))
 
+
 (define (apply-a-rule rule query-pattern query-frame)
   (let ((clean-rule (rename-variables-in rule)))
     (let ((unify-result (unify-match query-pattern
@@ -180,6 +181,7 @@
         (qeval (rule-body clean-rule)
                (singleton-stream unify-result))))))
 
+
 (define (rename-variables-in rule)
   (let ((rule-application-id (new-rule-application-id)))
     (define (tree-walk exp)
@@ -188,6 +190,7 @@
                                (tree-walk (cdr exp))))
             (else exp)))
     (tree-walk rule)))
+
 
 (define (unify-match p1 p2 frame)
   (cond ((eq? frame 'failed) 'failed)
@@ -200,6 +203,30 @@
                                                                (car p2)
                                                                frame)))
         (else 'failed)))
+
+
+(define (extend-if-possible var val frame)
+  (let ((binding (binding-in-frame var frame)))
+    (cond (binding (unify-match (binding-value binding) val frame))
+          ((var? val) (let ((binding (binding-in-frame val frame)))
+                        (if binding
+                          (unify-match var (binding-value binding) frame)
+                          (extend var val frame))))
+          ((depends-on? val var frame) 'failed)
+          (else (extend var val frame)))))
+
+(define (depends-on? exp var frame)
+  (define (tree-walk e)
+    (cond ((var? e) (if (equal? var e)
+                      true
+                      (let ((b (binding-in-frame e frame)))
+                        (if b
+                          (tree-walk (binding-value b))
+                          false))))
+          ((pair? e) (or (tree-walk (car e))
+                         (tree-walk (cdr e))))
+          (else false)))
+  (tree-walk exp))
 
 
 ;;
