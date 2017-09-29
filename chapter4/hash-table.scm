a513b93458b90d62872d5f5b9ef3b994ae13a44c
Grigory Bordyugov <gregbg@gmail.com>
2017-09-29 09:31:40 +0200

    added hash-table.scm

---
+++ chapter4/hash-table.scm
@@ -0,0 +1,9 @@
+(define make-hash-table make-equal-hash-table)
+
+(define *the-hash-table* (make-hash-table))
+
+(define (put op type datum)
+  (hash-table/put! *the-hash-table* (cons op type) datum))
+
+(define (get op type)
+  (hash-table/get *the-hash-table* (cons op type) '()))
