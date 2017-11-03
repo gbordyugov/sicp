9e0933e8efa60a981c44b40ae54926d5e2f5ecf8
Grigory Bordyugov <gregbg@gmail.com>
2017-11-03 09:40:22 +0100

    Mickey and Minnie

--- chapter4/testing-query-interpreter.scm
+++ chapter4/testing-query-interpreter.scm
@@ -7,3 +7,13 @@
 (unify-match (query-syntax-process '(?x a ?y))
              (query-syntax-process '(xx a ?y))
              '())
+
+;;
+;; Mickey and Minnie
+
+(assert!
+  (married Minnie Mickey))
+
+(assert!
+  (rule (married ?x ?y)
+        (married ?y ?x)))
