fe939ca40eac2af5ad3bd605e394d89d90038435
Grigory Bordyugov <gregbg@gmail.com>
2017-11-09 10:40:23 +0100

    reset-history!

--- chapter4/chapter4.04.scm
+++ chapter4/chapter4.04.scm
@@ -361,25 +361,24 @@
 (assert! (wife Lamech Ada))
 (assert! (son Ada Jabal))
 (assert! (son Ada Jubal))
+
 (assert!
   (rule (ends-in-grandson (grandson))))
 (assert!
   (rule (ends-in-grandson (?head . ?rest))
         (ends-in-grandson ?rest)))
+
 (assert!
   (rule ((great . ?rel) ?x ?y)
         (and (ends-in-grandson ?rel)
              (?rel ?z ?y)
              (son ?x ?z))))
 
-(append-to-form (a b) (c d)  ?z)
-(append-to-form (a b)  ?y   (a b c d))
-(append-to-form  ?x   (c d) (a b c d))
+(ends-in-grandson (grandson))
 
-((grandson) ?g ?ggs)
+(ends-in-grandson (great grandson))
 
-((great grandson) ?g ?ggs)
-
+(ends-in-grandson (great littlesn))
 
 ;;
 ;; exercise 4.70
