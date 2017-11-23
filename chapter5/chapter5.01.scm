7adb8188a2e4b6096e9aa6efaf2584a0a1babd51
Grigory Bordyugov <gregbg@gmail.com>
2017-11-23 09:47:05 +0100

    intro to Chapter 5

--- chapter5/chapter5.01.scm
+++ chapter5/chapter5.01.scm
@@ -1,3 +1,16 @@
 ;;
 ;; started Chapter 5
 ;;
+
+;;
+;; register machine -- a model for modern computers
+;;
+;; has a fixed set of storage elements, called registers
+;;
+;; sequentially executes instructions that manimulate those registers
+
+;;
+;; whereas addition and multiplication can be easily implemented in
+;; hardware, such operations as car, cdr, and cons need to know more
+;; about memory and how to allocate it
+;;
