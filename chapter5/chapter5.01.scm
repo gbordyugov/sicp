894b9194907536505c989481d4757fdc53b9a5e2
Grigory Bordyugov <gregbg@gmail.com>
2017-11-24 09:50:13 +0100

    GCD machine spec

--- chapter5/chapter5.01.scm
+++ chapter5/chapter5.01.scm
@@ -63,3 +63,30 @@
 ;;  - an unconditional branch (goto instruction)
 ;;
 
+;;
+;; A specification of the GCD machine
+;;
+
+(data-paths
+  (registers
+    ((name a)
+     (buttons ((name a<-b) (source (register b)))))
+    ((name b)
+     (buttons ((name b<-t) (source (register t)))))
+    ((name t)
+     (buttons ((name t<-r) (source (operation rem))))))
+  (operations
+    ((name rem)
+     (inputs (register a) (register b)))
+    ((name =)
+     (inputs (register b) (constant 0)))))
+
+(controller
+  test-b                          ;; label
+  (test =)                        ;; test
+  (branch (label gcd-done))       ;; conditional branch
+  (t<-r)                          ;; button push
+  (a<-b)                          ;; button push
+  (b<-t)                          ;; button push
+  (goto (label test-b))           ;; unconditional branch
+  gcd-done)
