4fc00e13cf98791477159c4354c0b3586cd3ff0d
Grigory Bordyugov <gregbg@gmail.com>
2017-12-07 09:39:41 +0100

    machine simulator interface spec

--- chapter5/chapter5.02.scm
+++ chapter5/chapter5.02.scm
@@ -1,3 +1,18 @@
 ;;
 ;; 5.2. A Register-Machine Simulator
 ;;
+
+
+;;
+;; (make-machine <register-names> <operations> <controller>)
+;; constructs a model of the machine
+;;
+;; (set-register-contents! <machine-model> <register-name> <value>)
+;; does what it's supposed to do
+;;
+;; (get-register-contents <machine-model> <register-name>)
+;; dito
+;;
+;; (start <machine-model>)
+;; simulates the execution of the given machine
+;;
