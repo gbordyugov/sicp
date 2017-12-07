c0164fc6c4e16588d64f3c9de3c79e82b95e4c40
Grigory Bordyugov <gregbg@gmail.com>
2017-12-07 09:50:42 +0100

    started chapter5/machine.scm

---
+++ chapter5/machine.scm
@@ -0,0 +1,14 @@
+;;
+;; Simulator of register machines of Chapter 5.2
+;;
+
+(define (make-machine register-names ops controller-text)
+  (let ((machine (make-new-machine)))
+    (for-each
+      (lambda (register-name)
+        ((machine 'allocate-register) register-name))
+      register-names)
+    ((machine 'install-operations) ops)
+    ((machine 'install-instruction-sequence)
+     (assemble controller-text machine))
+    machine))
