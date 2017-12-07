98f65b05f1c5576f3bb15de6ef8dbf1769c254fe
Grigory Bordyugov <gregbg@gmail.com>
2017-12-07 09:59:37 +0100

    stack of Chapter 5.2

--- chapter5/machine.scm
+++ chapter5/machine.scm
@@ -34,3 +34,35 @@
             (else
               (error "Unknown request: REGISTER" message))))
   dispatch))
+
+
+;;
+;; The stack
+;;
+
+;;
+;; This stack is also a procedure with local state
+;;
+
+(define (make-stack)
+  (let ((s '()))
+    (define (push x) (set! s (cons x s)))
+    (define (pop)
+      (if (null? s)
+        (error "Empty stack: POP")
+        (let ((top (car s)))
+          (set! s (cdr s))
+          top)))
+    (define (initialize)
+      (set! s '())
+      'done)
+    (define (dispatch message)
+      (cond ((eq? message 'push) push)
+            ((eq? message  'pop)  (pop))
+            ((eq? message 'initialize) (initialize))
+            (else
+              (error "Unknown request: STACK" message))))
+  dispatch))
+
+(define (pop stack) (stack 'pop))
+(define (push stack value) ((stack 'push) value))
