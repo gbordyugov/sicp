1363e795d7c4d9b7aae2cadcd52fe09cec6b0728
Grigory Bordyugov <gregbg@gmail.com>
2017-12-07 09:53:52 +0100

    registers of Chapter 5.2

--- chapter5/machine.scm
+++ chapter5/machine.scm
@@ -2,6 +2,9 @@
 ;; Simulator of register machines of Chapter 5.2
 ;;
 
+;;
+;; machines are procedures with local state, able to accept messages
+;;
 (define (make-machine register-names ops controller-text)
   (let ((machine (make-new-machine)))
     (for-each
@@ -12,3 +15,22 @@
     ((machine 'install-instruction-sequence)
      (assemble controller-text machine))
     machine))
+
+;;
+;; Registers
+;;
+
+;;
+;; registers are procedures with local state
+;;
+
+(define (make-register name)
+  (let ((contents '*unassigned*))
+    (define (dispatch message)
+      (cond ((eq? message 'get) contents)
+            ((eq? message 'set)
+             (lambda (value)
+               (set! contents value)))
+            (else
+              (error "Unknown request: REGISTER" message))))
+  dispatch))
