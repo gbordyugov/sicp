3f2fea26a30a9c4366096ca433766e8cd0459b2c
Grigory Bordyugov <gregbg@gmail.com>
2017-12-13 10:15:16 +0100

    assign procedure

--- chapter5/machine.scm
+++ chapter5/machine.scm
@@ -256,3 +256,19 @@
         (else
           (error "Unknown instruction type: ASSEMBLE" inst))))
 
+;;
+;; assign
+;;
+
+(define (make-assign inst machine labels operations pc)
+  (let ((target (get-register machine (assign-reg-name inst)))
+        (value-exp (assign-value-exp inst)))
+    (let ((value-proc
+            (if (operation-exp? value-exp)
+              (make-operation-exp value-exp
+                                  machine labels operations)
+              (make-primitive-exp (car value-exp) machine labels))))
+      (lambda () ;; execution procedure for assign
+        (set-contents! target (value-proc))
+        (advance-pc pc)))))
+
