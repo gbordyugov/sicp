bd6cc5311b6dd38fa698366d178bed006dc4521d
Grigory Bordyugov <gregbg@gmail.com>
2017-12-15 10:16:52 +0100

    Chapter 5.2.3: make-goto

--- chapter5/machine.scm
+++ chapter5/machine.scm
@@ -325,3 +325,22 @@
         (error "Bad BRANCH instruction: ASSEMBLE" inst))))
 (define (branch-dest branch-instruction)
   (cadr branch-instruction))
+
+;;
+;; make-goto
+;;
+
+(define (make-goto inst machine labels pc)
+  (let ((dest (goto-dest inst)))
+    (cond ((label-exp?  dest)
+           (let ((insts (lookup-label labels (label-exp-label dest))))
+             (lambda () (set-contents! pc insts))))
+          ((register-exp? dest)
+           (let ((reg (get-register machine (register-exp-reg dest))))
+             (lambda ()
+               (set-contents! pc (get-contents reg)))))
+          (else (error "Bad GOTO instruction: ASSEMBLE" inst)))))
+
+(define (goto-dest goto-instruction)
+  (cadr goto-instruction))
+
