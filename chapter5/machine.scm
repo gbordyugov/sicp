15bb0b1fe0e09d4446b2c99121e48a460e300592
Grigory Bordyugov <gregbg@gmail.com>
2017-12-15 10:22:22 +0100

    Chapter 5.2.3: make-save

--- chapter5/machine.scm
+++ chapter5/machine.scm
@@ -344,3 +344,12 @@
 (define (goto-dest goto-instruction)
   (cadr goto-instruction))
 
+;;
+;; Other instructions
+;;
+
+(define (make-save inst machine stack pc)
+  (let ((reg (get-register machine (stack-inst-reg-name inst))))
+    (lambda ()
+      (push stack (get-contents reg))
+      (advance-pc pc))))
