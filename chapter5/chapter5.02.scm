5f5653db895fa2757920df14f140056ee2680474
Grigory Bordyugov <gregbg@gmail.com>
2018-01-16 09:04:32 +0100

    exercise 5.11 (c): make-new-machine allocates a separate stack for each register

--- chapter5/chapter5.02.scm
+++ chapter5/chapter5.02.scm
@@ -202,3 +202,77 @@
 ;; 2. make push/pop use the corresponding register stack
 ;; 3. update initialize-stack to initialize stacks of all registers
 ;;
+
+;;
+;; 1.
+;;
+(define (make-new-machine)
+  (let ((pc    (make-register  'pc))
+        (flag  (make-register 'flag))
+        ;; (stack (make-stack))
+        (the-instruction-sequence '()))
+    (let ((the-ops
+            ;; those are the supported operations (such ass
+            ;; add/sub/mul/div)
+            (list (list 'initialize-stack
+                        ;; TODO!
+                        (lambda () (stack 'initialize)))))
+          (register-table
+            (list (list 'pc pc (make-stack))
+                  (list 'flag flag (make-stack)))))
+      ;;
+      ;; add new register
+      ;;
+      (define (allocate-register name)
+        (if (assoc name register-table)
+          (error "Multiply defined register: " name)
+          (set! register-table (cons (list name
+                                           (make-register name)
+                                           (make-stack))
+                                     register-table)))
+        'register-allocated)
+      ;;
+      ;; lookup register
+      ;;
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+            (cadr val)
+            (error "Unknown register: " name))))
+      (define (lookup-register-stack name)
+        (let ((val (assoc name register-table)))
+          (if val
+            (caddr val)
+            (error "Unknown register: " name))))
+      ;;
+      ;; run simulation
+      ;;
+      (define (execute)
+        (let ((insts (get-contents pc))) ;; those are the instructions
+          (if (null? insts)
+            'done
+            (begin
+              ;; note that his one is a function call with no
+              ;; arguments (aka execution procedure). By convention,
+              ;; execution procedure must advance programme counter pc
+              ((instruction-execution-proc (car insts)))
+              (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq)
+                 (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'get-register-stack) lookup-register-stack)
+              ;; this one adds new ops to the existing ones
+              ((eq? message 'install-operations)
+               (lambda (ops)
+                 (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+              (else
+                (error "Unknown request: MACHINE" message))))
+      dispatch)))
