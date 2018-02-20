d88983577749846f60546074a526efacebad65bd
Grigory Bordyugov <gregbg@gmail.com>
2018-02-20 10:34:10 +0100

    exercise 5.19: label-context

--- chapter5/chapter5.02.scm
+++ chapter5/chapter5.02.scm
@@ -681,3 +681,35 @@
 
 (define (set-instruction-execution-proc! inst proc)
   (set-car! (cdr inst) proc))
+
+;;
+;; a label context is a hash table with labels as keys and offset from
+;; those labels as values
+;;
+
+(define (make-label-context)
+  (make-equal-hash-table))
+
+(define (label-context-put-label-offset label-context label offset)
+  (hash-table/put! label-context label offset))
+
+(define (label-context-get-label-offset label-context label)
+  (hash-table/get label-context label #f))
+
+(define (label-context-inc-label-offset label-context label)
+  (let ((value (hash-table/get label-context label 0)))
+    (hash-table/put! label-context label (+ 1 value))))
+
+(define (label-context-inc-all-label-offsets label-context)
+  (let ((labels (hash-table/key-list label-context)))
+    (for-each (lambda (label)
+                (label-context-inc-label-offset label-context label))
+              labels)))
+
+(define label-context (make-label-context))
+(label-context-put-label-offset label-context 'label-1 1)
+(label-context-put-label-offset label-context 'label-2 2)
+
+(label-context-inc-all-label-offsets label-context)
+
+(hash-table->alist label-context)
