b3cf7d90b7037ad7aa59d71194067f7816c0ef37
Grigory Bordyugov <gregbg@gmail.com>
2018-03-20 10:07:33 +0100

    obarray and interning symbols

--- chapter5/chapter5.03.scm
+++ chapter5/chapter5.03.scm
@@ -65,3 +65,20 @@
 ;; the cdr of the initial par points to p2, which in turn is a number
 ;; 3 plus cdr pointing to p4, which is a car=n4 and cdr=e0
 ;;
+
+;;
+;; A symbol is represented as a typed pointer that designates a
+;; sequence of the characters that form the printed representation of
+;; the symbol
+;;
+;; In order to ensure that every symbol appears in the system only
+;; once, there is the `obarray' table that keeps the set of all
+;; symbols that have been seen so far. When a symbol is encountered by
+;; the reader, it's checked against obarray - if it's present, the
+;; reader get the address of the existing symbol, otherwise it creates
+;; a new one.
+;;
+;; The process of replacing character strings by unique pointers is
+;; called `interning' symbols
+;;
+
