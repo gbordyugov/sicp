702ed8ca45e9c551af295a22cf9f4a6f851b46b0
Grigory Bordyugov <gregbg@gmail.com>
2018-04-10 09:50:25 +0200

    5.4.1 The core of the Explicit-Control Evaluator

--- chapter5/chapter5.04.scm
+++ chapter5/chapter5.04.scm
@@ -22,3 +22,28 @@
 ;;     itself
 ;; proc, arg1, and unev are used in evaluating combinations.
 ;;
+
+;;
+;; 5.4.1 The core of the Explicit-Control Evaluator
+;;
+
+eval-dispatch
+  (test (op self-evaluating?) (reg exp))
+  (branch (label ev-self-eval))
+  (test (op variable?) (reg exp))
+  (branch (label ev-variable))
+  (test (op quoted?) (reg exp))
+  (branch (label ev-quoted))
+  (test (op assignment?) (reg exp))
+  (branch (label ev-assignment))
+  (test (op definition?) (reg exp))
+  (branch (label ev-definition))
+  (test (op if?) (reg exp))
+  (branch (label ev-if))
+  (test (op lambda?) (reg exp))
+  (branch (label eval-lambda))
+  (test (op begin?) (reg exp))
+  (branch (label ev-begin))
+  (test (op application?) (reg exp))
+  (branch (label ev-application))
+  (goto (label unknown-expression-type))
